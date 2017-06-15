14c84632b0b1be0c6dc5ccc3baf689fdaebcaef2
Ha Nhat Lenh <lenh@ktc.local>
2017-06-15 11:08:03 +0900

    Commited from lenh at 2017年 6月 15日 木曜日 11:08:03

--- modules/opts/server/models/opt.server.model.js
+++ modules/opts/server/models/opt.server.model.js
@@ -40,5 +40,15 @@ var OptSchema = new Schema({
     default: Date.now
   }
 });
+/**
+ * Hook a pre save method to hash the password
+ */
+OptSchema.pre('save', function (next) {
+  if (typeof this._id !== 'String') {
+    this._id = mongoose.Types.ObjectId(this._id);
+  }
+
+  next();
+});
 
 mongoose.model('Opt', OptSchema);

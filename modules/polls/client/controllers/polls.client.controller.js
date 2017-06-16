cf59b124cfdd1e5eb547e78998e02eb0ef5fb5af
Ha Nhat Lenh <lenh@ktc.local>
2017-06-16 19:18:43 +0900

    Commited from lenh at 2017年 6月 16日 金曜日 19:18:43

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -55,10 +55,34 @@
     vm.discard = discard;
     vm.comment = comment;
 
+    $scope.start = moment();
+    $scope.end = moment().add(1, 'days').add(1, 'hours');
 
-    // $scope.loadItems = function(query) {
-    //   return Tags.query().$promise;
-    // };
+    $scope.presets = [{
+      'name': 'This Week',
+      'start': moment().startOf('week').startOf('day'),
+      'end': moment().endOf('week').endOf('day'),
+    }, {
+      'name': 'This Month',
+      'start': moment().startOf('month').startOf('day'),
+      'end': moment().endOf('month').endOf('day'),
+    }, {
+      'name': 'This Year',
+      'start': moment().startOf('year').startOf('day'),
+      'end': moment().endOf('year').endOf('day'),
+    }];
+    $scope.changed = function() {
+      console.log('changed start or end datetime objects');
+    };
+    $scope.changedStart = function() {
+      console.log('changed start datetime object');
+    };
+    $scope.changedEnd = function() {
+      console.log('changed end datetime object');
+    };
+    $scope.closed = function() {
+      console.log('edit popover closed');
+    };
 
     // Remove existing Poll
     function remove() {

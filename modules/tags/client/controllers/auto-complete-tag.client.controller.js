e8dd0ff4f4fb63fe5455fb9d6c0683aac002d587
Ha Nhat Lenh <lenh@ktc.local>
2017-06-16 17:46:08 +0900

    Commited from lenh at 2017年 6月 16日 金曜日 17:46:08

--- modules/tags/client/controllers/auto-complete-tag.client.controller.js
+++ modules/tags/client/controllers/auto-complete-tag.client.controller.js
@@ -6,11 +6,22 @@
     .module('tags')
     .controller('AutocompleteTagController', AutocompleteTagController);
 
-  AutocompleteTagController.$inject = ['$scope', '$http', 'TagsService'];
+  AutocompleteTagController.$inject = ['$scope', '$http', '$q', 'TagsService'];
 
   function AutocompleteTagController($scope, $http, Tags) {
+    $scope.tags = Tags.query();
+
     $scope.loadItems = function(query) {
+      var items, deferred = $q.defer();
       return Tags.query().$promise;
+
+      items = _.chain($scope.tags),filter((obj) => {
+        return obj.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+      })
+      .take(10)
+      .value();
+      deferred.resolve(items);
+      return deferred.promise;
     };
     $scope.placeholder = 'Type your tags';
   }

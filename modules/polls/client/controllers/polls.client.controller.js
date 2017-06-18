3bbdac712a621efd5d9a12ae4a253c680f8856f2
acenter2507 <acenter2507@gmail.com>
2017-06-18 23:11:07 +0900

    Commited from HaNhatLenh at 2017年 6月 18日 日曜日 23:11:07

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -149,7 +149,7 @@
 
     function reply(cmt) {
       if (vm.authentication.user) {
-        vm.comment = (cmt) ? new Cmts() : cmt;
+        vm.comment = (!cmt) ? new Cmts() : cmt;
         $aside(vm.comment_form);
       } else {
         $state.go('authentication.signin');

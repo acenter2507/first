361671b91fd9ce13871a477a50a0150621bb47b9
acenter2507 <acenter2507@gmail.com>
2017-06-18 16:58:00 +0900

    Commited from HaNhatLenh at 2017年 6月 18日 日曜日 16:58:00

--- config/assets/default.js
+++ config/assets/default.js
@@ -4,17 +4,17 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/components-font-awesome/css/font-awesome.css',
+        // 'public/lib/bootstrap/dist/css/bootstrap.css',
+        // 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        // 'public/lib/components-font-awesome/css/font-awesome.css',
         'public/lib/angular-ui-switch/angular-ui-switch.css',
         'public/lib/ng-tags-input/ng-tags-input.css',
         'public/lib/angular-moment-picker/dist/angular-moment-picker.css'
       ],
       js: [
-        'public/lib/angular/angular.js',
+        // 'public/lib/angular/angular.js',
         'public/lib/angular-resource/angular-resource.js',
-        'public/lib/angular-animate/angular-animate.js',
+        // 'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
@@ -25,9 +25,9 @@ module.exports = {
         'public/lib/ng-tags-input/ng-tags-input.js',
         'public/lib/underscore/underscore.js',
         'public/lib/moment/moment.js',
-        'public/lib/angular-moment-picker/dist/angular-moment-picker.js',
-        'public/lib/angular-strap/dist/angular-strap.js',
-        'public/lib/angular-strap/dist/angular-strap.tpl.js'
+        'public/lib/angular-moment-picker/dist/angular-moment-picker.js'
+        // 'public/lib/angular-strap/dist/angular-strap.js',
+        // 'public/lib/angular-strap/dist/angular-strap.tpl.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },

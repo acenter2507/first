0b4b46ca7d874e3e7a0510173efd67238e9f4844
Ha Nhat Lenh <lenh@ktc.local>
2017-06-19 16:45:53 +0900

    Commited from lenh at 2017年 6月 19日 月曜日 16:45:53

--- config/assets/default.js
+++ config/assets/default.js
@@ -19,7 +19,7 @@ module.exports = {
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        // 'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/angular-strap/dist/angular-strap.js',
         'public/lib/angular-strap/dist/angular-strap.tpl.js',

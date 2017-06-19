e3c3d604103d10858a3c4d85c75a7e8052080472
Ha Nhat Lenh <lenh@ktc.local>
2017-06-19 16:53:35 +0900

    Commited from lenh at 2017年 6月 19日 月曜日 16:53:35

--- modules/polltags/client/config/polltags.client.config.js
+++ modules/polltags/client/config/polltags.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', {
-      title: 'Polltags',
-      state: 'polltags',
-      type: 'dropdown',
-      roles: ['*']
-    });
+    // Menus.addMenuItem('topbar', {
+    //   title: 'Polltags',
+    //   state: 'polltags',
+    //   type: 'dropdown',
+    //   roles: ['*']
+    // });
 
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'polltags', {
-      title: 'List Polltags',
-      state: 'polltags.list'
-    });
+    // // Add the dropdown list item
+    // Menus.addSubMenuItem('topbar', 'polltags', {
+    //   title: 'List Polltags',
+    //   state: 'polltags.list'
+    // });
 
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'polltags', {
-      title: 'Create Polltag',
-      state: 'polltags.create',
-      roles: ['admin']
-    });
+    // // Add the dropdown create item
+    // Menus.addSubMenuItem('topbar', 'polltags', {
+    //   title: 'Create Polltag',
+    //   state: 'polltags.create',
+    //   roles: ['admin']
+    // });
   }
 }());
--- modules/tags/client/config/tags.client.config.js
+++ modules/tags/client/config/tags.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', {
-      title: 'Tags',
-      state: 'tags',
-      type: 'dropdown',
-      roles: ['*']
-    });
+    // Menus.addMenuItem('topbar', {
+    //   title: 'Tags',
+    //   state: 'tags',
+    //   type: 'dropdown',
+    //   roles: ['*']
+    // });
 
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'tags', {
-      title: 'List Tags',
-      state: 'tags.list'
-    });
+    // // Add the dropdown list item
+    // Menus.addSubMenuItem('topbar', 'tags', {
+    //   title: 'List Tags',
+    //   state: 'tags.list'
+    // });
 
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'tags', {
-      title: 'Create Tag',
-      state: 'tags.create',
-      roles: ['user']
-    });
+    // // Add the dropdown create item
+    // Menus.addSubMenuItem('topbar', 'tags', {
+    //   title: 'Create Tag',
+    //   state: 'tags.create',
+    //   roles: ['user']
+    // });
   }
 }());

27bf6a2d63cb7267cdc7b498cc042f45d94cd9d3
Ha Nhat Lenh <lenh@ktc.local>
2017-06-20 14:47:19 +0900

    Commited from lenh at 2017年 6月 20日 火曜日 14:47:19

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -250,7 +250,7 @@
       return _.contains(vm.votedOpts, id);
     };
     vm.voted_title = function() {
-      return _.pluck(_.filter(vm.opts, function(opt){ return _.contains(vm.votedOpts, opt._id); }));
+      return _.pluck(_.filter(vm.opts, function(opt){ return _.contains(vm.votedOpts, opt._id); }), 'title');
     }
     function send_vote() {
       if (!vm.votedOpts.length || vm.votedOpts.length === 0) {

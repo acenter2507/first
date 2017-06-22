9132f08e3fc13bed96407553bff3a9d77de2e487
Ha Nhat Lenh <lenh@ktc.local>
2017-06-22 20:24:47 +0900

    Commited from lenh at 2017年 6月 22日 木曜日 20:24:47

--- modules/polls/client/controllers/poll-input.client.controller.js
+++ modules/polls/client/controllers/poll-input.client.controller.js
@@ -53,8 +53,12 @@
         $state.go('poll.list');
       });
       Socket.on('poll_update', (res) => {
-        vm.poll = Polls.get({ pollId: vm.poll._id }).$promise.then(res => {
+        Polls.get({ pollId: vm.poll._id }, (_poll) => {
+          vm.poll = _poll;
+          vm.poll.close = (vm.poll.close) ? moment(vm.poll.close) : vm.poll.close;
+          vm.poll.tags = [];
           loadOpts();
+          loadTags();
         });
       });
       Socket.on('opts_update', (res) => {

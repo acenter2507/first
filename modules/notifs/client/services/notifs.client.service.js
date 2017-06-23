e78497fcfae04a08abd9eb91c6bba04851abce9d
Ha Nhat Lenh <lenh@ktc.local>
2017-06-23 16:39:30 +0900

    Commited from lenh at Fri, Jun 23, 2017  4:39:30 PM

--- modules/notifs/client/services/notifs.client.service.js
+++ modules/notifs/client/services/notifs.client.service.js
@@ -1,26 +1,25 @@
 // Notifs service used to communicate Notifs REST endpoints
-(function () {
+(function() {
   'use strict';
-
-  angular
-    .module('notifs')
-    .factory('NotifsService', NotifsService);
+  angular.module('notifs').factory('NotifsService', NotifsService);
 
   NotifsService.$inject = ['$resource'];
 
   function NotifsService($resource) {
-    return $resource('api/notifs/:notifId', {
-      notifId: '@_id'
-    }, {
+    return $resource(
+      'api/notifs/:notifId',
+      {
+        notifId: '@_id'
+      },
+      {
         update: {
           method: 'PUT'
         }
-      });
+      }
+    );
   }
 
-  angular
-    .module('notifs')
-    .factory('NotifsApi', NotifsApi);
+  angular.module('notifs').factory('NotifsApi', NotifsApi);
 
   NotifsApi.$inject = ['$http'];
 
@@ -31,4 +30,4 @@
       }
     };
   }
-}());
+})();

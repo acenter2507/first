49daa868d2c2a01761683235b5076faaa3123ca0
Ha Nhat Lenh <lenh@ktc.local>
2017-06-23 15:25:37 +0900

    Commited from lenh at Fri, Jun 23, 2017  3:25:37 PM

--- modules/core/server/sockets/core.server.socket.config.js
+++ modules/core/server/sockets/core.server.socket.config.js
@@ -6,11 +6,13 @@ module.exports = function (io, socket) {
   socket.on('init', function (req) {
     if (!_.contains(global.socketUsers, { socket: socket.id })) {
       global.socketUsers.push({ socket: socket.id, user: req.user });
+      console.log('Has user online: ', global.socketUsers.length);
     }
   });
 
   // Emit the status event when a socket client is disconnected
   socket.on('disconnect', function () {
     global.socketUsers = _.without(global.socketUsers, { socket: socket.id });
+    console.log('Has user offline: ', global.socketUsers.length);
   });
 };

441b98d27b6c543bada10df52b72056ccd921838
Ha Nhat Lenh <lenh@ktc.local>
2017-06-23 16:32:52 +0900

    Commited from lenh at Fri, Jun 23, 2017  4:32:52 PM

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus', 'Socket', 'NotifsService', 'NotifsApi'
+angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus', 'Socket', 'NotifsService', 'NotifsApi',
   function($scope, $state, Authentication, Menus, Socket, Notifs, NotifsApi) {
     // Expose view variables
     $scope.$state = $state;
@@ -11,12 +11,12 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
 
     // Toggle the menu items
     $scope.isCollapsed = false;
-    $scope.toggleCollapsibleMenu = function() {
+    $scope.toggleCollapsibleMenu = function () {
       $scope.isCollapsed = !$scope.isCollapsed;
     };
 
     // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function() {
+    $scope.$on('$stateChangeSuccess', function () {
       $scope.isCollapsed = false;
     });
 

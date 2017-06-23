b2ce056dc44bc3d9bb82819ca4eef9e407011907
Ha Nhat Lenh <lenh@ktc.local>
2017-06-23 14:58:01 +0900

    Commited from lenh at Fri, Jun 23, 2017  2:58:00 PM

--- modules/core/server/sockets/core.server.socket.config.js
+++ modules/core/server/sockets/core.server.socket.config.js
@@ -2,13 +2,19 @@
 
 const _ = require('lodash');
 // Create the chat configuration
-module.exports = function(io, socket) {
-  socket.on('init', function(req) {
+module.exports = function (io, socket) {
+  socket.on('init', function (req) {
     console.log('Has user join system');
+    if (!_.constant(global.socketUsers, { socket: socket.id })) {
+      global.socketUsers.push({ socket: socket.id, user: req.user });
+    }
+    console.log('Online users: ', global.socketUsers.length);
   });
 
   // Emit the status event when a socket client is disconnected
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
+    global.socketUsers = _.without(global.socketUsers, { socket: socket.id });
     console.log('Has user out system');
+    console.log('Online users: ', global.socketUsers.length);
   });
 };

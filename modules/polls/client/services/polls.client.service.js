02dacd90444cffe31f3e0c3e3d86eddd40cc32e8
acenter2507 <acenter2507@gmail.com>
2017-06-24 11:11:40 +0900

    Commited from HaNhatLenh at Sat, Jun 24, 2017 11:11:40 AM

--- modules/polls/client/services/polls.client.service.js
+++ modules/polls/client/services/polls.client.service.js
@@ -1,26 +1,25 @@
 // Polls service used to communicate Polls REST endpoints
-(function () {
+(function() {
   'use strict';
-
-  angular
-    .module('polls')
-    .factory('PollsService', PollsService);
+  angular.module('polls').factory('PollsService', PollsService);
 
   PollsService.$inject = ['$resource'];
 
   function PollsService($resource) {
-    return $resource('api/polls/:pollId', {
-      pollId: '@_id'
-    }, {
+    return $resource(
+      'api/polls/:pollId',
+      {
+        pollId: '@_id'
+      },
+      {
         update: {
           method: 'PUT'
         }
-      });
+      }
+    );
   }
 
-  angular
-    .module('polls')
-    .factory('PollsApi', PollsApi);
+  angular.module('polls').factory('PollsApi', PollsApi);
 
   PollsApi.$inject = ['$http'];
 
@@ -52,4 +51,4 @@
       }
     };
   }
-}());
+})();

f036f62d71a71efd9836d992e66e76c56dfa9dbf
acenter2507 <acenter2507@gmail.com>
2017-06-26 23:50:19 +0900

    Commited from HaNhatLenh at Mon, Jun 26, 2017 11:50:19 PM

--- test.js
+++ test.js
@@ -14,10 +14,11 @@ app.init(function () {
 
 
 // 1. Khi người vote là guest thì không push socket được.
-// 2. Sửa các progressbar trong màn hình detail.
+// 2. Sửa các progressbar trong màn hình detail. => Đã sửa xong
 // 3. Tạo tooltip
 // 3. Lọc notif
-// 4. Infinity scroll
+// 4. Infinity scroll => Đã xong màn hình list poll và màn hình view poll (cần nghiên cứu sort)
 // 5. Chức năng bookmark
 // 6. Thêm tùy chọn vote multiple
 // 7. Chức năng private và share link
+// 8. Thêm nút bấm đổi kiểu biểu đồ tròn

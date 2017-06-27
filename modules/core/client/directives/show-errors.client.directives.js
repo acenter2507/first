a57a1e97e1543e62d137d26ac1d71913398cc31f
acenter2507 <acenter2507@gmail.com>
2017-06-28 00:58:00 +0900

    Commited from HaNhatLenh at Wed, Jun 28, 2017 12:58:00 AM

--- modules/core/client/directives/show-errors.client.directives.js
+++ modules/core/client/directives/show-errors.client.directives.js
@@ -111,10 +111,10 @@ angular
     return function(msg, is_xhtml) {
       var _is_xhtml = is_xhtml || true;
       var breakTag = _is_xhtml ? '<br />' : '<br>';
-      var msg = (msg + '').replace(
+      var _msg = (msg + '').replace(
         /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
         '$1' + breakTag + '$2'
       );
-      return $sce.trustAsHtml(msg);
+      return $sce.trustAsHtml(_msg);
     };
   });

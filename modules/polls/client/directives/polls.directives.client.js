0496a42866cc4af05067133c899e85097bfa7402
acenter2507 <acenter2507@gmail.com>
2017-06-27 23:04:39 +0900

    Commited from HaNhatLenh at Tue, Jun 27, 2017 11:04:39 PM

--- modules/polls/client/directives/polls.directives.client.js
+++ modules/polls/client/directives/polls.directives.client.js
@@ -1,8 +1,6 @@
 (function() {
   'use strict';
-  angular
-    .module('polls')
-    .directive('ngEnter', ngEnter);
+  angular.module('polls').directive('ngEnter', ngEnter);
 
   function ngEnter() {
     return function(scope, element, attrs) {
@@ -19,19 +17,32 @@
     };
   }
 
-  angular
-    .module('polls').directive('focusMe', function($timeout) {
-      return {
-        scope: { trigger: '@focusMe' },
-        link: function(scope, element) {
-          scope.$watch('trigger', function(value) {
-            if (value === 'true') {
-              $timeout(function() {
-                element[0].focus();
-              });
-            }
-          });
-        }
-      };
-    });
-}());
+  angular.module('polls').directive('focusMe', function($timeout) {
+    return {
+      scope: { trigger: '@focusMe' },
+      link: function(scope, element) {
+        scope.$watch('trigger', function(value) {
+          if (value === 'true') {
+            $timeout(function() {
+              element[0].focus();
+            });
+          }
+        });
+      }
+    };
+  });
+
+  angular.module('polls').directive('autoAdjust', function($filter) {
+    return {
+      restrict: 'AEC',
+      scope: {
+        value: '=ngBind'
+      },
+      link: function($scope, element, attrs) {
+        var width = element[0].offsetWidth,
+          charactersLimit = width / 12,
+          text = $filter('splice')($scope.value, charactersLimit, '...');
+      }
+    };
+  });
+})();

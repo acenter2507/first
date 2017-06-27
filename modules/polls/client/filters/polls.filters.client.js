1eb39c4754a0c91deca0f8317d98f73853ee7055
acenter2507 <acenter2507@gmail.com>
2017-06-28 00:49:15 +0900

    Commited from HaNhatLenh at Wed, Jun 28, 2017 12:49:15 AM

--- modules/polls/client/filters/polls.filters.client.js
+++ modules/polls/client/filters/polls.filters.client.js
@@ -3,16 +3,6 @@
   // Làm cho text tự cắt chiều dài hợp với div
   angular
     .module('polls')
-    .filter('splice', function() {
-      return function(value, max, tail) {
-        if (!value) return '';
-        max = parseInt(max, 10);
-        if (!max) return value;
-        if (value.length <= max) return value;
-        value = value.substr(0, max);
-        return value + (tail || ' …');
-      };
-    })
     // Chuyển đổi nội dung comment giữ nguyên line break
     .filter('nl2br', function($sce) {
       return function(msg, is_xhtml) {

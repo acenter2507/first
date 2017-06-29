0a5c9fd8d77dc5282eb84893fc6527a6caae33c7
Ha Nhat Lenh <lenh@ktc.local>
2017-06-29 11:44:07 +0900

    Commited from lenh at Thu, Jun 29, 2017 11:44:07 AM

--- modules/opts/server/policies/opts.server.policy.js
+++ modules/opts/server/policies/opts.server.policy.js
@@ -28,7 +28,7 @@ exports.invokeRolesPolicies = function () {
       permissions: ['get', 'post']
     }, {
       resources: '/api/opts/:optId',
-      permissions: ['get']
+      permissions: ['get', 'put']
     }]
   }, {
     roles: ['guest'],

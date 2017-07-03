e40ebfc8c14ca63b9436a4e7e0c4fc9da1be67b0
Ha Nhat Lenh <lenh@ktc.local>
2017-07-03 17:59:11 +0900

    Commited from lenh at Mon, Jul 03, 2017  5:59:11 PM

--- modules/categorys/client/config/categorys.client.config.js
+++ modules/categorys/client/config/categorys.client.config.js
@@ -13,20 +13,21 @@
       title: 'Categorys',
       state: 'categorys',
       type: 'dropdown',
-      roles: ['*']
+      roles: ['admin']
     });
 
     // Add the dropdown list item
     menuService.addSubMenuItem('topbar', 'categorys', {
       title: 'List Categorys',
-      state: 'categorys.list'
+      state: 'categorys.list',
+      roles: ['admin']
     });
 
     // Add the dropdown create item
     menuService.addSubMenuItem('topbar', 'categorys', {
       title: 'Create Category',
       state: 'categorys.create',
-      roles: ['user']
+      roles: ['admin']
     });
   }
 }());

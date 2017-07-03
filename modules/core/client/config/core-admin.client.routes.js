e5775c56745c2c6e548a9d81a3a0152737a294f7
acenter2507 <acenter2507@gmail.com>
2017-07-03 21:59:42 +0900

    Commited from HaNhatLenh at Mon, Jul 03, 2017  9:59:42 PM

--- modules/core/client/config/core-admin.client.routes.js
+++ modules/core/client/config/core-admin.client.routes.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // Setting up route
-// angular.module('core.admin.routes').config(['$stateProvider',
-angular.module('core').config(['$stateProvider',
+angular.module('core.admin.routes').config(['$stateProvider',
+// angular.module('core').config(['$stateProvider',
   function ($stateProvider) {
+    console.log('config admis');
     $stateProvider
       .state('admins', {
         abstract: true,

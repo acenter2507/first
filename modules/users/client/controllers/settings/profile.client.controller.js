213eb68c61077d9d0cdd6a610eea4d433d201c93
Ha Nhat Lenh <lenh@ktc.local>
2017-07-04 11:51:59 +0900

    Commited from lenh at Tue, Jul 04, 2017 11:51:59 AM

--- modules/users/client/controllers/settings/profile.client.controller.js
+++ modules/users/client/controllers/settings/profile.client.controller.js
@@ -9,5 +9,6 @@ angular.module('users').controller('ProfileController', [
   'userResolve',
   function ($scope, $http, $location, Users, Authentication, user) {
     $scope.user = user;
+    $scope.isCurrentOwner = user._id === Authentication.user._id;
   }
 ]);

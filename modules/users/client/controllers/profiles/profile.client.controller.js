e2ac14e286195b49d48a956f36559edc68ac55b4
Ha Nhat Lenh <lenh@ktc.local>
2017-07-12 16:55:04 +0900

    Commited from lenh at Wed, Jul 12, 2017  4:55:04 PM

--- modules/users/client/controllers/profiles/profile.client.controller.js
+++ modules/users/client/controllers/profiles/profile.client.controller.js
@@ -12,12 +12,10 @@ angular.module('users').controller('ProfileController', [
     $scope.isCurrentOwner = profile._id === $scope.user._id;
     $scope.isLogged = ($scope.user) ? true : false;
 
-    var count_view = $timeout(() => {
-      if (!$scope.isCurrentOwner) {
-        var _user = new Users($scope.user);
-        _user.viewCnt += 1;
-        _user.$update();
-      }
-    }, 3000);
+    if ($scope.isLogged && !$scope.isCurrentOwner) {
+      var _user = new Users($scope.user);
+      _user.viewCnt += 1;
+      _user.$update(console.log('count_up'));
+    }
   }
 ]);

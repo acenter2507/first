92db6e2ce0782ba1c8907f4ca23904690bfe2dc5
Ha Nhat Lenh <lenh@ktc.local>
2017-07-12 10:40:24 +0900

    Commited from lenh at Wed, Jul 12, 2017 10:40:24 AM

--- modules/core/client/config/core-plugin.config.js
+++ modules/core/client/config/core-plugin.config.js
@@ -21,47 +21,42 @@ angular.module('core').config(['$provide',
   function ($provide) {
     $provide.decorator('taOptions', ['taRegisterTool', 'taToolFunctions', 'ngDialog', '$delegate',
       function (taRegisterTool, taToolFunctions, dialog, taOptions) {
-        // $delegate is the taOptions we are decorating
-        // here we override the default toolbars specified in taOptions.
-        // taOptions.toolbar = [
-        //   ['clear', 'h1', 'h2', 'h3'],
-        //   ['ul', 'ol'],
-        //   ['bold', 'italics'],
-        //   ['insertLink', 'insertVideo']
-        // ];
-
         taRegisterTool('uploadImage', {
-        iconclass: "fa fa-picture-o",
-        tooltiptext: 'Upload an image',
-        onElementSelect: {
-          element: 'img',
-          action: taToolFunctions.imgOnSelectAction
-        },
-        action: function() {
-          var $editor = this.$editor;
+          iconclass: "fa fa-upload",
+          tooltiptext: 'Upload an image',
+          onElementSelect: {
+            element: 'img',
+            action: taToolFunctions.imgOnSelectAction
+          },
+          action: function () {
+            var $editor = this.$editor;
 
-          // Create a virtual input element.
-          var input = document.createElement('input');
-          input.type = 'file';
-          input.accept = "image/*";
+            // Create a virtual input element.
+            var input = document.createElement('input');
+            input.type = 'file';
+            input.accept = "image/*";
 
-          input.onchange = function() {
-            var reader = new FileReader();
+            input.onchange = function () {
+              var reader = new FileReader();
 
-            if (this.files && this.files[0]) {
-              reader.onload = function(e) {
-                $editor().wrapSelection('insertHtml', '<img src=' + e.target.result + '>', true);
-              };
-
-              reader.readAsDataURL(this.files[0]);
-            }
-          };
-
-          // Click on a virtual input element.
-          input.click();
-        }
-      });
-        taOptions.toolbar[3].push('uploadImage');
+              if (this.files && this.files[0]) {
+                reader.onload = function (e) {
+                  $editor().wrapSelection('insertHtml', '<img src=' + e.target.result + '>', true);
+                };
+                reader.readAsDataURL(this.files[0]);
+              }
+            };
+            // Click on a virtual input element.
+            input.click();
+          }
+        });
+        taOptions.toolbar = [
+          ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+          ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+          ['html', 'insertImage', 'insertLink', 'insertVideo', 'uploadImage', 'wordcount', 'charcount']
+        ];
+        // taOptions.toolbar[3].push('uploadImage');
         return taOptions;
       }
     ]);

14693098c14cd51ba0886efda4e8678ba4df16d9
Ha Nhat Lenh <lenh@ktc.local>
2017-07-12 10:29:09 +0900

    Commited from lenh at Wed, Jul 12, 2017 10:29:09 AM

--- modules/core/client/config/core-plugin.config.js
+++ modules/core/client/config/core-plugin.config.js
@@ -23,70 +23,14 @@ angular.module('core').config(['$provide',
       function (taRegisterTool, taToolFunctions, dialog, taOptions) {
         // $delegate is the taOptions we are decorating
         // here we override the default toolbars specified in taOptions.
-        taOptions.toolbar = [
-          ['clear', 'h1', 'h2', 'h3'],
-          ['ul', 'ol'],
-          ['bold', 'italics'],
-          ['insertLink', 'insertVideo']
-        ];
+        // taOptions.toolbar = [
+        //   ['clear', 'h1', 'h2', 'h3'],
+        //   ['ul', 'ol'],
+        //   ['bold', 'italics'],
+        //   ['insertLink', 'insertVideo']
+        // ];
 
-        // Create our own insertImage button
-        taRegisterTool('customInsertImage', {
-          iconclass: "fa fa-picture-o",
-          action: function () {
-            var textAngular = this;
-
-            // var savedSelection = rangy.saveSelection();
-            // var modalInstance = $modal.open({
-            //   // Put a link to your template here or whatever
-            //   template: '<label>Enter the url to your image:</label><input type="text" ng-model="img.url"><button ng-click="submit()">OK</button>',
-            //   size: 'sm',
-            //   controller: ['$modalInstance', '$scope',
-            //     function ($modalInstance, $scope) {
-            //       $scope.img = {
-            //         url: ''
-            //       };
-            //       $scope.submit = function () {
-            //         $modalInstance.close($scope.img.url);
-            //       };
-            //     }
-            //   ]
-            // });
-
-            // modalInstance.result.then(function (imgUrl) {
-            //   rangy.restoreSelection(savedSelection);
-            //   textAngular.$editor().wrapSelection('insertImage', imgUrl);
-            // });
-            return false;
-          },
-        });
         taRegisterTool('uploadImage', {
-          buttontext: 'Select Image',
-          iconclass: "fa fa-image",
-          action: function (deferred, restoreSelection) {
-            var self = this;
-            dialog.openConfirm({
-              // controller: 'PollInputController',
-              templateUrl: 'modules/core/client/views/templates/confirm.dialog.template.html'
-            }).then(confirm => {
-              alert(1);
-            }, reject => {
-            });
-            console.log('Clicked');
-            // $mdDialog.show({
-            //   templateUrl: 'uploadImagesCont.tmpl.html',
-            //   parent: angular.element(document.body),
-            //   //controller: textAngularUploadImage,
-            //   clickOutsideToClose: false
-            // }).then(function (result) {
-            //   sel.collapseToEnd();
-            //   self.$editor().wrapSelection('insertImage', $rootScope.ImageResult);
-            //   deferred.resolve();
-            // });
-            return false;
-          }
-        });
-        taRegisterTool('uploadImage2', {
         iconclass: "fa fa-picture-o",
         tooltiptext: 'Upload an image',
         onElementSelect: {
@@ -117,7 +61,7 @@ angular.module('core').config(['$provide',
           input.click();
         }
       });
-        taOptions.toolbar[3].push('customInsertImage', 'uploadImage', 'uploadImage2');
+        taOptions.toolbar[3].push('uploadImage');
         return taOptions;
       }
     ]);

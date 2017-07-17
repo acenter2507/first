2638ca279c4f84274a1b505620ffc9898a9e5176
acenter2507 <acenter2507@gmail.com>
2017-07-17 16:41:57 +0900

    Commited from HaNhatLenh at Mon, Jul 17, 2017  4:41:56 PM

--- modules/categorys/client/controllers/list-categorys.client.controller.js
+++ modules/categorys/client/controllers/list-categorys.client.controller.js
@@ -5,11 +5,26 @@
     .module('categorys')
     .controller('CategorysListController', CategorysListController);
 
-  CategorysListController.$inject = ['CategorysService'];
+  CategorysListController.$inject = [
+    'CategorysService',
+    'Action'
+  ];
 
-  function CategorysListController(CategorysService) {
+  function CategorysListController(CategorysService, Action) {
     var vm = this;
 
-    vm.categorys = CategorysService.query();
+    var promise = CategorysService.query().$promise;
+    promise.then(_categorys => {
+      vm.categorys = _categorys || [];
+      vm.categorys.forEach(function (category) {
+        Action.count_poll_for_category(category._id)
+          .then(res => {
+            category.count = res.data || 0;
+          })
+          .catch(err => {
+            toast.error(err.message, 'Error!');
+          });
+      }, this);
+    });
   }
 }());

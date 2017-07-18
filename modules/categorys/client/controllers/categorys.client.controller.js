986944cc47a3191610853bb059973a836a0905eb
acenter2507 <acenter2507@gmail.com>
2017-07-18 19:19:07 +0900

    Commited from HaNhatLenh at Tue, Jul 18, 2017  7:19:06 PM

--- modules/categorys/client/controllers/categorys.client.controller.js
+++ modules/categorys/client/controllers/categorys.client.controller.js
@@ -36,169 +36,13 @@
 
     vm.category = category;
 
-    // Infinity scroll
-    vm.stopped = false;
-    vm.busy = false;
-    vm.page = 0;
-    vm.sort = '-created';
-    vm.new_data = [];
-    vm.polls = [];
-
-    vm.get_polls = get_polls;
-    function get_polls() {
-      if (vm.stopped || vm.busy) return;
-      vm.busy = true;
-      Action.get_category_polls(vm.category._id, vm.page, vm.sort)
-        .then(res => {
-          if (!res.data.length || res.data.length === 0) {
-            vm.stopped = true;
-            vm.busy = false;
-            return;
-          }
-          // Load options và tính vote cho các opt trong polls
-          vm.new_data = res.data || [];
-          var promises = [];
-          vm.new_data.forEach(poll => {
-            poll.isCurrentUserOwner = vm.isLogged && vm.user._id === poll.user._id;
-            promises.push(get_poll_report(poll));
-            promises.push(get_opts(poll));
-            promises.push(get_owner_follow(poll));
-            promises.push(get_reported(poll));
-            promises.push(get_bookmarked(poll));
-          });
-          return Promise.all(promises);
-        })
-        .then(res => {
-          // Gán data vào list hiện tại
-          vm.polls = _.union(vm.polls, vm.new_data);
-          vm.page += 1;
-          vm.busy = false;
-          vm.new_data = [];
-        })
-        .catch(err => {
-          vm.busy = false;
-          vm.stopped = true;
-          toast.error(err.message, 'Error!');
-        });
-    }
-
-    function get_poll_report(poll) {
-      return new Promise((resolve, reject) => {
-        Action.get_poll_report(poll._id)
-          .then(res => {
-            poll.report = res.data;
-            return resolve(res);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    function get_opts(poll) {
-      return new Promise((resolve, reject) => {
-        Action.get_opts(poll._id)
-          .then(res => {
-            poll.opts = _.where(res.data, { status: 1 }) || [];
-            return get_vote_for_poll(poll);
-          })
-          .then(res => {
-            return resolve(poll);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    function get_vote_for_poll(poll) {
-      return new Promise((resolve, reject) => {
-        Action.get_voteopts(poll._id)
-          .then(res => {
-            poll.chart = {
-              options: { responsive: true },
-              colors: [],
-              labels: [],
-              data: []
-            };
-            poll.votes = res.data.votes || [];
-            poll.voteopts = res.data.voteopts || [];
-            poll.total = poll.voteopts.length;
-            poll.opts.forEach(opt => {
-              opt.voteCnt = _.where(poll.voteopts, { opt: opt._id }).length || 0;
-              opt.progressVal = calPercen(poll.total, opt.voteCnt);
-              poll.chart.data.push(opt.voteCnt);
-              poll.chart.colors.push(opt.color);
-              poll.chart.labels.push(opt.title);
-            });
-            return resolve(poll);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    function get_owner_follow(poll) {
-      return new Promise((resolve, reject) => {
-        if (!vm.isLogged) {
-          poll.follow = {};
-          return resolve();
-        }
-        Action.get_follow(poll._id)
-          .then(res => {
-            poll.follow = res.data || { poll: poll._id };
-            return resolve(res.data);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    function get_reported(poll) {
-      return new Promise((resolve, reject) => {
-        if (!vm.isLogged) {
-          poll.reported = false;
-          return resolve();
-        }
-        Action.get_report(poll._id)
-          .then(res => {
-            poll.reported = (res.data) ? res.data : false;
-            return resolve(res.data);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    function get_bookmarked(poll) {
-      return new Promise((resolve, reject) => {
-        if (!vm.isLogged) {
-          poll.bookmarked = false;
-          return resolve();
-        }
-        Action.get_bookmark(poll._id)
-          .then(res => {
-            poll.bookmarked = (res.data) ? res.data : false;
-            return resolve(res.data);
-          })
-          .catch(err => {
-            return reject(err);
-          });
-      });
-    }
-    // Tính phần trăm tỉ lệ vote cho opt
-    function calPercen(total, value) {
-      if (total === 0) {
-        return 0;
-      }
-      return Math.floor(value * 100 / total) || 0;
-    }
-
-    vm.delete_poll = (poll) => {
-      if (!poll.isCurrentUserOwner) {
+    vm.remove = () => {
+      if (!vm.isAdmin) {
         toast.error('You are not authorized.', 'Error!');
         return;
       }
-      $scope.message_title = 'Delete poll!';
-      $scope.message_content = 'Are you sure you want to delete this poll?';
+      $scope.message_title = 'Delete category!';
+      $scope.message_content = 'Are you sure you want to delete this category?';
       $scope.dialog_type = 3;
       $scope.buton_label = 'delete';
       dialog.openConfirm({
@@ -209,54 +53,11 @@
       }, reject => {
       });
       function handle_delete() {
-        vm.polls = _.without(vm.polls, poll);
-        Action.delete_poll(poll);
+        Action.delete_category(vm.category)
+          .then(() => {
+            $state.go('categorys.list');
+          });
       }
-    };
-    vm.report_poll = (poll) => {
-      if (poll.reported) {
-        toast.error('You are already report this poll.', 'Error!');
-        return;
-      }
-      Action.save_report(poll._id)
-        .then(res => {
-          poll.reported = (res) ? true : false;
-          $scope.$apply();
-          toast.success('You have successfully reported this poll.', 'Thank you!');
-        })
-        .catch(err => {
-          toast.error(err.message, 'Error!');
-        });
-    };
-    vm.bookmark_poll = (poll) => {
-      if (poll.bookmarked) {
-        toast.error('You are already bookmark this poll.', 'Error!');
-        return;
-      }
-      Action.save_bookmark(poll._id)
-        .then(res => {
-          poll.bookmarked = (res) ? true : false;
-          $scope.$apply();
-          toast.success('Added to bookmarks.', 'Success!');
-        })
-        .catch(err => {
-          toast.error(err.message, 'Error!');
-        });
-    };
-    vm.follow_poll = (poll) => {
-      if (!vm.isLogged) {
-        toast.error('You must login to follow this poll.', 'Error!');
-        return;
-      }
-      Action.save_follow(poll.follow)
-        .then(res => {
-          poll.follow = res;
-          $scope.$apply();
-          toast.success('You followed this poll.', 'Success!');
-        })
-        .catch(err => {
-          toast.error(err.message, 'Error!');
-        });
     };
   }
 }());

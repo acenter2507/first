251446b91280ff41c6bd122d9f646d8e35088681
acenter2507 <acenter2507@gmail.com>
2017-07-20 23:59:42 +0900

    Commited from HaNhatLenh at Thu, Jul 20, 2017 11:59:42 PM

--- modules/polls/client/controllers/search.client.controller.js
+++ modules/polls/client/controllers/search.client.controller.js
@@ -63,7 +63,8 @@ angular.module('polls').controller('PollsSearchController', [
     $scope.clear_preferences = () => {
       $scope.condition = {};
       Storages.set_local(Constants.storages.preferences, JSON.stringify($scope.condition));
-      $state.go('search');
+      // $state.go('search');
+      $location.url($location.path())
     };
     $scope.save_preferences = () => {
       Storages.set_local(Constants.storages.preferences, JSON.stringify($scope.condition));

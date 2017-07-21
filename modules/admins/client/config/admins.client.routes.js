b358bce09bd3ec98795002f9cb58ac5aaee4bd12
acenter2507 <acenter2507@gmail.com>
2017-07-21 23:12:01 +0900

    Commited from HaNhatLenh at Fri, Jul 21, 2017 11:12:01 PM

--- modules/admins/client/config/admins.client.routes.js
+++ modules/admins/client/config/admins.client.routes.js
@@ -9,14 +9,29 @@ angular.module('admin.routes').config(['$stateProvider',
         templateUrl: 'modules/admins/client/views/dashboard.client.view.html',
         controller: 'DashboardController'
       })
+      .state('admin.polls', {
+        url: '/polls',
+        templateUrl: 'modules/admins/client/views/polls/list-polls.client.view.html',
+        controller: 'PollListController'
+      })
       .state('admin.users', {
         url: '/users',
-        templateUrl: 'modules/admins/client/views/list-users.client.view.html',
+        templateUrl: 'modules/admins/client/views/users/list-users.client.view.html',
         controller: 'UserListController'
+      })
+      .state('admin.user-new', {
+        url: '/users/new',
+        templateUrl: 'modules/admins/client/views/users/new-user.client.view.html',
+        controller: 'UserController',
+        resolve: {
+          userResolve: ['Admin', function (Admin) {
+            return new Admin();
+          }]
+        }
       })
       .state('admin.user', {
         url: '/users/:userId',
-        templateUrl: 'modules/admins/client/views/view-user.client.view.html',
+        templateUrl: 'modules/admins/client/views/users/view-user.client.view.html',
         controller: 'UserController',
         resolve: {
           userResolve: ['$stateParams', 'Admin', function ($stateParams, Admin) {
@@ -28,7 +43,7 @@ angular.module('admin.routes').config(['$stateProvider',
       })
       .state('admin.user-edit', {
         url: '/users/:userId/edit',
-        templateUrl: 'modules/admins/client/views/edit-user.client.view.html',
+        templateUrl: 'modules/admins/client/views/users/edit-user.client.view.html',
         controller: 'UserController',
         resolve: {
           userResolve: ['$stateParams', 'Admin', function ($stateParams, Admin) {
---
+++ modules/admins/client/controllers/polls/list-polls.client.controller.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+  angular
+    .module('admin')
+    .controller('PollListController', PollListController);
+
+  PollListController.$inject = [
+    '$state',
+    '$scope',
+    'Authentication'
+  ];
+
+  function PollListController(
+    $state,
+    $scope,
+    Authentication
+  ) {
+    // Code
+  }
+})();

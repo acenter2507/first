b4f66ce217678c279bab9e9a6f3ceeae59e04b4d
acenter2507 <acenter2507@gmail.com>
2017-07-22 22:53:10 +0900

    Commited from HaNhatLenh at Sat, Jul 22, 2017 10:53:10 PM

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -43,11 +43,6 @@ angular.module('core').controller('HeaderController', [
         loadUncheckNotifs();
         initSocket();
       }
-      $('.dropdown.search-dropdown').on({
-        "shown.bs.dropdown": function () { this.closable = false; },
-        "click": function () { this.closable = true; },
-        "hide.bs.dropdown": function () { return this.closable; }
-      });
     }
 
     function initSocket() {

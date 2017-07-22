31d142431499d312a7a28e3460b5b62ba2595853
acenter2507 <acenter2507@gmail.com>
2017-07-22 20:35:19 +0900

    Commited from HaNhatLenh at Sat, Jul 22, 2017  8:35:19 PM

--- modules/admins/client/controllers/dashboard.client.controller.js
+++ modules/admins/client/controllers/dashboard.client.controller.js
@@ -15,9 +15,56 @@
     $scope,
     Authentication
   ) {
-    $scope.total_users = 2500;
-    $scope.total_polls = 4981;
-    $scope.total_comments = 2500;
-    $scope.total_visit = 24230;
+    $scope.today = 
+    $scope.users_chart = {};
+    $scope.visit_chart = {};
+    $scope.polls_chart = {};
+    $scope.cmts_chart = {};
+
+    function init_user_chart() {
+      
+    }
+    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    $scope.data = [
+      [65, 59, 84, 84, 51, 55, 40]
+    ];
+    $scope.colors = [{
+      backgroundColor: brandPrimary,
+      borderColor: 'rgba(255,255,255,.55)',
+    }];
+    $scope.options = {
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [{
+          gridLines: {
+            color: 'transparent',
+            zeroLineColor: 'transparent'
+          },
+          ticks: {
+            fontSize: 2,
+            fontColor: 'transparent',
+          }
+
+        }],
+        yAxes: [{
+          display: false,
+          ticks: {
+            display: false,
+            min: Math.min.apply(Math, $scope.data[0]) - 5,
+            max: Math.max.apply(Math, $scope.data[0]) + 5,
+          }
+        }],
+      },
+      elements: {
+        line: {
+          borderWidth: 1
+        },
+        point: {
+          radius: 4,
+          hitRadius: 10,
+          hoverRadius: 4,
+        },
+      },
+    }
   }
 })();

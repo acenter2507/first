071962a27f91dc2415a7882aaee3b134b02d392f
Lenh Ha Nhat <hanhatlenh@Lenhs-MacBook-Pro.local>
2017-07-22 20:36:34 +0900

    Commited from hanhatlenh at Sat Jul 22 20:36:34 JST 2017

--- modules/admins/client/controllers/dashboard.client.controller.js
+++ modules/admins/client/controllers/dashboard.client.controller.js
@@ -15,56 +15,68 @@
     $scope,
     Authentication
   ) {
-    $scope.today = 
-    $scope.users_chart = {};
-    $scope.visit_chart = {};
+    var brandPrimary =  '#20a8d8';
+    var brandSuccess =  '#4dbd74';
+    var brandInfo =     '#63c2de';
+    var brandWarning =  '#f8cb00';
+    var brandDanger =   '#f86c6b';
+
+    var grayDark =      '#2a2c36';
+    var gray =          '#55595c';
+    var grayLight =     '#818a91';
+    var grayLighter =   '#d1d4d7';
+    var grayLightest =  '#f8f9fa';
+
+    $scope.users_chart = init_usersChart();
     $scope.polls_chart = {};
     $scope.cmts_chart = {};
+    $scope.visit_chart = {};
 
-    function init_user_chart() {
-      
-    }
-    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    $scope.data = [
-      [65, 59, 84, 84, 51, 55, 40]
-    ];
-    $scope.colors = [{
-      backgroundColor: brandPrimary,
-      borderColor: 'rgba(255,255,255,.55)',
-    }];
-    $scope.options = {
-      maintainAspectRatio: false,
-      scales: {
-        xAxes: [{
-          gridLines: {
-            color: 'transparent',
-            zeroLineColor: 'transparent'
-          },
-          ticks: {
-            fontSize: 2,
-            fontColor: 'transparent',
-          }
-
-        }],
-        yAxes: [{
-          display: false,
-          ticks: {
+    function init_usersChart() {
+      var chart = {};
+      chart.mode = 'day'; // week, month, year
+      chart.labels = ['January','February','March','April','May','June','July'];
+      chart.data = [
+        [65, 59, 84, 84, 51, 55, 40]
+      ];
+      chart.colors = [{
+        backgroundColor: brandPrimary,
+        borderColor: 'rgba(255,255,255,.55)',
+      }];
+      chart.options = {
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            gridLines: {
+              color: 'transparent',
+              zeroLineColor: 'transparent'
+            },
+            ticks: {
+              fontSize: 2,
+              fontColor: 'transparent',
+            }
+          }],
+          yAxes: [{
             display: false,
-            min: Math.min.apply(Math, $scope.data[0]) - 5,
-            max: Math.max.apply(Math, $scope.data[0]) + 5,
-          }
-        }],
-      },
-      elements: {
-        line: {
-          borderWidth: 1
+            ticks: {
+              display: false,
+              min: Math.min.apply(Math, $scope.data[0]) - 5,
+              max: Math.max.apply(Math, $scope.data[0]) + 5,
+            }
+          }],
         },
-        point: {
-          radius: 4,
-          hitRadius: 10,
-          hoverRadius: 4,
+        elements: {
+          line: {
+            borderWidth: 1
+          },
+          point: {
+            radius: 4,
+            hitRadius: 10,
+            hoverRadius: 4,
+          },
         },
-      },
+      };
+      return chart;
     }
   }
 })();

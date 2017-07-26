85109ae79bac9aa20dec08f23a817c70d3a956ca
Ha Nhat Lenh <lenh@ktc.local>
2017-07-26 10:59:20 +0900

    Commited from lenh at Wed, Jul 26, 2017 10:59:20 AM

--- modules/cmtlikes/server/controllers/cmtlikes.server.controller.js
+++ modules/cmtlikes/server/controllers/cmtlikes.server.controller.js
@@ -58,7 +58,7 @@ exports.update = function(req, res) {
   cmtlike.save()
     .then(_cmtlike => {
       cmtlike = _cmtlike;
-      return Cmt.countLike(_cmtlike.cmt, cnt);
+      return Cmt.countLike(_cmtlike.cmt._id, cnt);
     }, handleError)
     .then(cmt => {
       res.jsonp({ like: cmtlike, likeCnt: cmt.likeCnt });

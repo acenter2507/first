b4e98a6b9a3ba09905c4dfbf76890fdcbec22f18
Ha Nhat Lenh <lenh@ktc.local>
2017-08-10 21:00:28 +0900

    Commited from lenh at Thu, Aug 10, 2017  9:00:27 PM

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -5,7 +5,7 @@ angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfig
 
 // Setting HTML5 Location Mode
 angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider', '$httpProvider',
-  function ($locationProvider, $httpProvider, webStorage) {
+  function ($locationProvider, $httpProvider) {
     $locationProvider.html5Mode(true).hashPrefix('!');
     $httpProvider.interceptors.push('authInterceptor');
   }

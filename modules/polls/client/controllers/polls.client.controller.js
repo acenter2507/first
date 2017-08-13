f7040f057d9366216983f5cf0aae1002268cc903
Ha Nhat Lenh <lenh@ktc.local>
2017-08-14 08:59:04 +0900

    Commited from lenh at Mon, Aug 14, 2017  8:59:04 AM

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -384,8 +384,8 @@
     }
     // Random code share poll
     function make_code() {
-      var text = "";
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      var text = '';
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       for (var i = 0; i < 10; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
       return text;

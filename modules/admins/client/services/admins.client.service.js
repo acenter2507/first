e961b7d3ab69a5e7bf3b0a02b502edc27263000d
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 14:30:35 +0900

    Commited from lenh at Tue, Aug 15, 2017  2:30:34 PM

--- modules/admins/client/services/admins.client.service.js
+++ modules/admins/client/services/admins.client.service.js
@@ -17,31 +17,49 @@ function admin($resource) {
 AdminApi.$inject = ['$http'];
 function AdminApi($http) {
   this.get_users = () => {
-    return $http.get('/api/admins/users');
+    return $http.get('/api/admins/users', {
+      ignoreLoadingBar: true
+    });
   };
   this.reset_pass = (userId, pass) => {
-    return $http.post('/api/admins/users/' + userId + '/resetpass', { password: pass });
+    return $http.post('/api/admins/users/' + userId + '/resetpass', { password: pass }, {
+      ignoreLoadingBar: true
+    });
   };
   this.get_polls_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/polls');
+    return $http.get('/api/admins/users/' + userId + '/polls', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_cmts_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/cmts');
+    return $http.get('/api/admins/users/' + userId + '/cmts', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_votes_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/votes');
+    return $http.get('/api/admins/users/' + userId + '/votes', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_reports_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/reports');
+    return $http.get('/api/admins/users/' + userId + '/reports', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_bereports_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/bereports');
+    return $http.get('/api/admins/users/' + userId + '/bereports', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_suggests_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/suggests');
+    return $http.get('/api/admins/users/' + userId + '/suggests', {
+      ignoreLoadingBar: true
+    });
   };
   this.get_logins_by_user = userId => {
-    return $http.get('/api/admins/users/' + userId + '/logins');
+    return $http.get('/api/admins/users/' + userId + '/logins', {
+      ignoreLoadingBar: true
+    });
   };
   return this;
 }

0622596d23c69927d974f5e44b28f9e9a1cfe7bd
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 11:58:00 +0900

    Commited from lenh at Tue, Aug 15, 2017 11:57:59 AM

--- modules/core/client/controllers/web-app.client.controller.js
+++ modules/core/client/controllers/web-app.client.controller.js
@@ -3,6 +3,7 @@
 angular.module('core').controller('WebAppController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
-    $scope.authentication = Authentication;
+    // $scope.authentication = Authentication;
+    $scope.title = 'xxxxxxxx';
   }
 ]);

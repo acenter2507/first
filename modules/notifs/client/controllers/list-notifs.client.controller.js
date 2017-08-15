b2106fd3fcf0b3e564e019c39af09dbede1671cd
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 09:09:59 +0900

    Commited from lenh at Tue, Aug 15, 2017  9:09:59 AM

--- modules/notifs/client/controllers/list-notifs.client.controller.js
+++ modules/notifs/client/controllers/list-notifs.client.controller.js
@@ -6,6 +6,7 @@
     .controller('NotifsListController', NotifsListController);
 
   NotifsListController.$inject = [
+    '$scope',
     'NotifsApi',
     'Authentication',
     '$filter',
@@ -14,6 +15,7 @@
   ];
 
   function NotifsListController(
+    $scope,
     NotifsApi,
     Authentication,
     $filter,

df1795376dee1c7ab6ae309718a3b04b8c10bf00
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 12:23:35 +0900

    Commited from lenh at Tue, Aug 15, 2017 12:23:35 PM

--- modules/core/client/controllers/web-app.client.controller.js
+++ modules/core/client/controllers/web-app.client.controller.js
@@ -3,9 +3,9 @@
 angular.module('core').controller('WebAppController', ['$rootScope', '$scope', 'Authentication',
   function ($rootScope, $scope, Authentication) {
     $scope.page_title = 'Polls';
-    
-    $rootScope.$on('updateNotif', notifs => {
-      if (notifs > 0) {
+
+    $rootScope.$on('updateNotif', function (event, data) {
+      if (data > 0) {
         $scope.page_title = '(' + notifs + ')' + $scope.page_title;
         console.log($scope.page_title);
       }

0ecb2a2e7806270dc381947f4ab2816ad8db48d4
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 13:04:41 +0900

    Commited from lenh at Tue, Aug 15, 2017  1:04:41 PM

--- modules/core/client/controllers/web-app.client.controller.js
+++ modules/core/client/controllers/web-app.client.controller.js
@@ -7,6 +7,7 @@ angular.module('core').controller('WebAppController', ['$rootScope', '$scope', '
     $scope.page_title = $scope.page_notifs + $scope.page_name;
 
     $rootScope.$on('updateNotif', function (event, data) {
+      console.log(data);
       if (data > 0) {
         $scope.page_notifs = '(' + data + ') ';
       } else {

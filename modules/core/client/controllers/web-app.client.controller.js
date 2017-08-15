0c8933ac8e386473b8e9603b7466a5512e41ccbf
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 12:18:16 +0900

    Commited from lenh at Tue, Aug 15, 2017 12:18:16 PM

--- modules/core/client/controllers/web-app.client.controller.js
+++ modules/core/client/controllers/web-app.client.controller.js
@@ -7,6 +7,7 @@ angular.module('core').controller('WebAppController', ['$rootScope', '$scope', '
     $rootScope.$on('updateNotif', notifs => {
       if (notifs > 0) {
         $scope.page_title = '(' + notifs + ')' + $scope.page_title;
+        console.log($scope.page_title);
       }
     });
   }

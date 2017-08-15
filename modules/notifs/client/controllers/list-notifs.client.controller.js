f04197f2516c3d13b433a1013d4a0768392d6a70
Ha Nhat Lenh <lenh@ktc.local>
2017-08-15 09:45:48 +0900

    Commited from lenh at Tue, Aug 15, 2017  9:45:48 AM

--- modules/notifs/client/controllers/list-notifs.client.controller.js
+++ modules/notifs/client/controllers/list-notifs.client.controller.js
@@ -41,6 +41,7 @@
             return;
           }
           vm.notifs = _.union(vm.notifs, res.data);
+          console.log(vm.notifs);
           $scope.page += 1;
           $scope.busy = false;
         })

66072557c3df4453d4691cfcbe9878d8a17be71d
acenter2507 <acenter2507@gmail.com>
2017-08-16 23:53:05 +0900

    Commited from HaNhatLenh at Wed, Aug 16, 2017 11:53:05 PM

--- modules/core/client/directives/common.client.directives.js
+++ modules/core/client/directives/common.client.directives.js
@@ -65,7 +65,7 @@ function backToTopDirective() {
   var directive = {
     restrict: 'E',
     replace: true,
-    template: '<button class="back-to-top"><i class="fa fa-chevron-up"></i></button>',
+    template: '<md-button md-colors="{background: \'blue\'}" aria-label="menu" class="md-fab md-mini"><i class="fa fa-chevron-up"></i></md-button>',
     link: link
   };
   return directive;

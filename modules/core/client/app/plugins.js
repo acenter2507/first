37f57c94728a9c3abb346a086e3e8704a22ee096
acenter2507 <acenter2507@gmail.com>
2017-08-19 02:11:27 +0900

    Commited from HaNhatLenh at Sat, Aug 19, 2017  2:11:27 AM

--- modules/core/client/app/plugins.js
+++ modules/core/client/app/plugins.js
@@ -50,7 +50,7 @@
               if (this.files && this.files[0]) {
                 reader.onload = function (e) {
                   console.log(e.target.result);
-                  $editor().wrapSelection('insertHtml', '<img src=' + e.target.result + ' with="100% height="50%""><div><br/></div>', true);
+                  $editor().wrapSelection('insertHtml', '<img src="' + e.target.result + '" with="100% height="50%""><div><br/></div>', true);
                 };
                 reader.readAsDataURL(this.files[0]);
               }

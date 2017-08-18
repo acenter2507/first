af1595c9cf0ec7baf1488d1dae3b35c9ae0e7073
Ha Nhat Lenh <lenh@ktc.local>
2017-08-18 17:00:42 +0900

    Commited from lenh at Fri, Aug 18, 2017  5:00:42 PM

--- modules/polls/client/controllers/poll-input.client.controller.js
+++ modules/polls/client/controllers/poll-input.client.controller.js
@@ -311,6 +311,7 @@
       }
       if (!ctrl.tmp_opt._id && !_.contains(ctrl.opts, ctrl.tmp_opt)) {
         ctrl.opts.push(ctrl.tmp_opt);
+        ctrl.tmp_opt = {};
       }
     };
     ctrl.opt_full = () => {

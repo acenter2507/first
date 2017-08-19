b04cee2d4c9400bd922ba0b754fbdae589567f17
acenter2507 <acenter2507@gmail.com>
2017-08-20 02:38:06 +0900

    Commited from HaNhatLenh at Sun, Aug 20, 2017  2:38:06 AM

--- modules/core/client/controllers/web-app.client.controller.js
+++ modules/core/client/controllers/web-app.client.controller.js
@@ -1,15 +1,22 @@
 'use strict';
 
-angular.module('core').controller('WebAppController', ['$rootScope', '$scope', 'Authentication',
-  function ($rootScope, $scope, Authentication) {
+angular.module('core').controller('WebAppController', ['$rootScope', '$scope', 'Authentication', 'Notification',
+  function ($rootScope, $scope, Authentication, Notification) {
     console.log('WebAppController');
+    // User info
     $scope.user = Authentication.user;
     $scope.isLogged = ($scope.user);
     $scope.isAdmin = $scope.isLogged && _.contains($scope.user.roles, 'admin');
+
+    // Notificaton info
+    if ($scope.isLogged) {
+      $scope.notifCnt = Notification.notifCnt;
+      $scope.notifications = Notification.notifications;
+    }
   
     $scope.page_notifs = '';
     $scope.page_name = 'Polls';
-    $scope.page_title = $scope.page_notifs + $scope.page_name;
+    $scope.page_title = ($scope.notifCnt > 0) ? '(' + $scope.notifCnt + ')' : '' + $scope.page_name;
 
     $rootScope.$on('updateNotif', function (event, data) {
       if (data > 0) {

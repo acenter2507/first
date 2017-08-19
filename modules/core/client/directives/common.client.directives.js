e924166ddcc0b113020a9eee6823a937c7bd03f0
acenter2507 <acenter2507@gmail.com>
2017-08-19 14:03:37 +0900

    Commited from HaNhatLenh at Sat, Aug 19, 2017  2:03:37 PM

--- modules/core/client/directives/common.client.directives.js
+++ modules/core/client/directives/common.client.directives.js
@@ -7,7 +7,8 @@ angular
   .directive('body', asideMenuHideDirective)
   .directive('backToTop', backToTopDirective)
   .directive('button', asidePanelToggleDirective)
-  .directive('a', asidePanelToggleDirective);
+  .directive('a', asidePanelToggleDirective)
+  .directive('imagePreview', imagePreviewDirective);
 
 //Prevent click if href="#"
 function preventClickDirective() {
@@ -119,3 +120,51 @@ function asidePanelToggleDirective() {
     });
   }
 }
+imagePreviewDirective.$inject = ['$window'];
+function imagePreviewDirective($window) {
+  var helper = {
+    support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+    isFile: function (item) {
+      return angular.isObject(item) && item instanceof $window.File;
+    },
+    isImage: function (file) {
+      var type = '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+      return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+    }
+  };
+  var directive = {
+    restrict: 'A',
+    template: '<canvas/>',
+    link: link
+  };
+  return directive;
+
+  function link(scope, element, attributes) {
+    if (!helper.support) return;
+
+    var params = scope.$eval(attributes.imagePreview);
+
+    if (!helper.isFile(params.file)) return;
+    if (!helper.isImage(params.file)) return;
+
+    var canvas = element.find('canvas');
+    var reader = new FileReader();
+
+    reader.onload = onLoadFile;
+    reader.readAsDataURL(params.file);
+
+    function onLoadFile(event) {
+      var img = new Image();
+      img.onload = onLoadImage;
+      img.src = event.target.result;
+    }
+
+    function onLoadImage() {
+      var width = params.width || this.width / this.height * params.height;
+      var height = params.height || this.height / this.width * params.width;
+      canvas.attr({ width: width, height: height });
+      canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+    }
+  }
+}
+

fab72cdcee5ce0a03a30d484ebf40f306ddbf6fb
acenter2507 <acenter2507@gmail.com>
2017-08-19 15:12:38 +0900

    Commited from HaNhatLenh at Sat, Aug 19, 2017  3:12:38 PM

--- modules/core/client/controllers/upload-images.client.controller.js
+++ modules/core/client/controllers/upload-images.client.controller.js
@@ -13,6 +13,7 @@
     $scope,
     FileUploader
   ) {
+    $scope.results = [];
     $scope.uploader = new FileUploader({
       url: 'api/polls/images',
       alias: 'imagesUpload'
@@ -24,32 +25,33 @@
         return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
       }
     });
-    $scope.uploader.onAfterAddingAll = function (addedFileItems) {
-      console.info('onAfterAddingAll', addedFileItems);
-    };
+    // $scope.uploader.onAfterAddingAll = function (addedFileItems) {
+    //   console.info('onAfterAddingAll', addedFileItems);
+    // };
     $scope.uploader.onBeforeUploadItem = function (item) {
-      console.info('onBeforeUploadItem', item);
+      // console.info('onBeforeUploadItem', item);
+      $scope.uploadingFileName = item._file.name;
     };
-    $scope.uploader.onProgressItem = function (fileItem, progress) {
-      console.info('onProgressItem', fileItem, progress);
-    };
-    $scope.uploader.onProgressAll = function (progress) {
-      console.info('onProgressAll', progress);
-    };
-    $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
-      console.info('onSuccessItem', fileItem, response, status, headers);
-    };
-    $scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
-      console.info('onErrorItem', fileItem, response, status, headers);
-    };
-    $scope.uploader.onCancelItem = function (fileItem, response, status, headers) {
-      console.info('onCancelItem', fileItem, response, status, headers);
-    };
+    // $scope.uploader.onProgressItem = function (fileItem, progress) {
+    //   console.info('onProgressItem', fileItem, progress);
+    // };
+    // $scope.uploader.onProgressAll = function (progress) {
+    //   console.info('onProgressAll', progress);
+    // };
+    // $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
+    //   console.info('onSuccessItem', fileItem, response, status, headers);
+    // };
+    // $scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
+    //   console.info('onErrorItem', fileItem, response, status, headers);
+    // };
+    // $scope.uploader.onCancelItem = function (fileItem, response, status, headers) {
+    //   console.info('onCancelItem', fileItem, response, status, headers);
+    // };
     $scope.uploader.onCompleteItem = function (fileItem, response, status, headers) {
       console.info('onCompleteItem', fileItem, response, status, headers);
     };
     $scope.uploader.onCompleteAll = function () {
-      console.info('onCompleteAll');
+      closeThisDialog('This my data');
     };
     $scope.btn_upload_click = () => {
       angular.element('#fileInput').trigger('click');

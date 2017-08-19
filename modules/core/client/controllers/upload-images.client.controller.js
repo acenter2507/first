42e4b47c60fe8111e0df3914637a1ecea12b23f9
acenter2507 <acenter2507@gmail.com>
2017-08-19 13:03:49 +0900

    Commited from HaNhatLenh at Sat, Aug 19, 2017  1:03:48 PM

--- modules/core/client/controllers/upload-images.client.controller.js
+++ modules/core/client/controllers/upload-images.client.controller.js
@@ -14,33 +14,33 @@
     FileUploader
   ) {
     console.log('Called controller');
-    var uploader = new FileUploader({
+    $scope.uploader = new FileUploader({
       url: 'api/polls/images',
-      alias: 'pollImageUpload'
+      alias: 'ImageUpload'
     });
-    uploader.filters.push({
+    $scope.uploader.filters.push({
       name: 'imageFilter',
       fn: function (item, options) {
         var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
         return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
       }
     });
-    uploader.onAfterAddingAll = function (addedFileItems) {
+    $scope.uploader.onAfterAddingAll = function (addedFileItems) {
       console.info('onAfterAddingAll', addedFileItems);
     };
-    uploader.onSuccessItem = function (fileItem, response, status, headers) {
+    $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
       console.info('onSuccessItem', fileItem, response, status, headers);
     };
-    uploader.onErrorItem = function (fileItem, response, status, headers) {
+    $scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
       console.info('onErrorItem', fileItem, response, status, headers);
     };
-    uploader.onCancelItem = function (fileItem, response, status, headers) {
+    $scope.uploader.onCancelItem = function (fileItem, response, status, headers) {
       console.info('onCancelItem', fileItem, response, status, headers);
     };
-    uploader.onCompleteItem = function (fileItem, response, status, headers) {
+    $scope.uploader.onCompleteItem = function (fileItem, response, status, headers) {
       console.info('onCompleteItem', fileItem, response, status, headers);
     };
-    uploader.onCompleteAll = function () {
+    $scope.uploader.onCompleteAll = function () {
       console.info('onCompleteAll');
     };
     console.log('Button upload clicked');

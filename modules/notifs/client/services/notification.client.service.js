a42a809cf2e7921367d563b7f622050d6b78a46c
acenter2507 <acenter2507@gmail.com>
2017-08-20 11:24:12 +0900

    Commited from HaNhatLenh at Sun, Aug 20, 2017 11:24:12 AM

--- modules/notifs/client/services/notification.client.service.js
+++ modules/notifs/client/services/notification.client.service.js
@@ -4,35 +4,35 @@ angular.module('core').service('Notification', Notification);
 
 Notification.$inject = ['$http', 'NotifsService'];
 function Notification($http, NotifsService) {
-  this.notifCnt = 0;
-  this.notifications = [];
+  var notifCnt = 0;
+  var notifications = [];
   this.loadNotifs = function () {
     $http.get('/api/notifs/load', { ignoreLoadingBar: true })
       .then(res => {
-        this.notifCnt = res.data.count || 0;
-        this.notifications = res.data.notifs || 0;
+        notifCnt = res.data.count || 0;
+        notifications = res.data.notifs || 0;
       });
   };
   this.clearNotifs = function () {
     $http.get('/api/clearAll', { ignoreLoadingBar: true })
       .then(res => {
-        this.notifCnt = 0;
-        this.notifications = [];
+        notifCnt = 0;
+        notifications = [];
       });
   };
   this.markReadNotifs = function () {
     $http.get('/api/markAllRead', { ignoreLoadingBar: true })
       .then(res => {
-        this.notifications.forEach(function (notif) {
+        notifications.forEach(function (notif) {
           notif.status = 1;
         });
       });
   };
   this.markReadNotif = function (notifId) {
-    var ntf = _.find(this.notifications, function (item) { return item._id.toString() === notifId.toString(); });
+    var ntf = _.find(notifications, function (item) { return item._id.toString() === notifId.toString(); });
     if (ntf) {
       ntf.status = 1;
-      this.notifCnt -= 1;
+      notifCnt -= 1;
     }
     let rs_notf = new NotifsService({ _id: notifId });
     rs_notf.status = 1;

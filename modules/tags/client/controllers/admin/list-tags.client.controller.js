3c098547e8b6839d5bcb8472d058e2a38aeb41a8
acenter2507 <acenter2507@gmail.com>
2017-08-21 23:45:50 +0900

    Commited from HaNhatLenh at Mon, Aug 21, 2017 11:45:50 PM

--- modules/tags/client/controllers/admin/list-tags.client.controller.js
+++ modules/tags/client/controllers/admin/list-tags.client.controller.js
@@ -55,6 +55,7 @@
     $scope.uploader = new FileUploader();
     $scope.uploader.onAfterAddingAll = function (addedFileItems) {
       var file = addedFileItems[0];
+      console.log(file);
       var reader = new FileReader();
       reader.onload = function (progressEvent) {
         // By lines

1a026794aa817b3fd94ac615b1a0d314ae2373cb
acenter2507 <acenter2507@gmail.com>
2017-08-22 14:59:50 +0900

    Commited from HaNhatLenh at Tue, Aug 22, 2017  2:59:50 PM

--- modules/core/client/services/activity.client.service.js
+++ modules/core/client/services/activity.client.service.js
@@ -7,6 +7,7 @@ function Activitys(Socket) {
   this.list = [];
   this.add = function (activity) {
     var time = new Date().toString();
+    console.log(time);
     activity.time = time;
     this.list.push(activity);
   };

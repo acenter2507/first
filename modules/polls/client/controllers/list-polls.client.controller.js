b09af3bdda9e5dda37a86e49209f4ca76189ee04
Ha Nhat Lenh <lenh@ktc.local>
2017-08-24 19:49:41 +0900

    Commited from lenh at Thu, Aug 24, 2017  7:49:41 PM

--- modules/polls/client/controllers/list-polls.client.controller.js
+++ modules/polls/client/controllers/list-polls.client.controller.js
@@ -244,13 +244,13 @@
       //   //   }
       //   // }
       // });
-      // var q_quick_poll = dialog.open({
-      //   template: 'modules/polls/client/views/quick-poll.client.view.html',
-      //   controller: 'QuickPollController',
-      //   controllerAs: 'ctrl',
-      //   appendClassName: 'images-upload-dialog',
-      //   closeByDocument: false
-      // });
+      var q_quick_poll = dialog.open({
+        templateUrl: '/tpl.html',
+        controller: 'QuickPollController',
+        controllerAs: 'ctrl',
+        appendClassName: 'images-upload-dialog',
+        closeByDocument: false
+      });
     };
     $scope.q_post_poll = () => {
       $state.go('polls.create');

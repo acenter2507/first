72cb05aa8a2febe5e741cf6846bbd3d667bb2ef9
Ha Nhat Lenh <lenh@ktc.local>
2017-08-24 14:49:23 +0900

    Commited from lenh at Thu, Aug 24, 2017  2:49:22 PM

--- modules/polls/client/controllers/quick-poll.client.controller.js
+++ modules/polls/client/controllers/quick-poll.client.controller.js
@@ -44,33 +44,15 @@
         ctrl.message = 'Please check your options, has invalid info';
         return false;
       }
-      console.log(ctrl.poll);
-
-      // if (!ctrl.poll.isPublic) {
-      //   $scope.message_title = 'Save poll!';
-      //   $scope.message_content = 'You want to save a private poll?';
-      //   $scope.dialog_type = 1;
-      //   $scope.buton_label = 'Save';
-      //   dialog.openConfirm({
-      //     scope: $scope,
-      //     templateUrl: 'modules/core/client/views/templates/confirm.dialog.template.html'
-      //   }).then(confirm => {
-      //     handle_save();
-      //   }, reject => {
-      //   });
-      // } else {
-      //   handle_save();
-      // }
-      // function handle_save() {
-      //   ctrl.poll.opts = ctrl.opts;
-      //   Action.save_poll(ctrl.poll)
-      //     .then(res => {
-      //       $state.go('polls.view', { pollId: res.slug });
-      //     })
-      //     .catch(err => {
-      //       toast.error(err.message, 'Error!');
-      //     });
-      // }
+      Action.save_poll(ctrl.poll)
+        .then(res => {
+          $state.go('polls.view', { pollId: res.slug });
+          $scope.closeThisDialog();
+        })
+        .catch(err => {
+          $scope.closeThisDialog();
+          toast.error('There were problems creating your poll.', 'Error!');
+        });
     };
     ctrl.add = () => {
       var color = randomColor();

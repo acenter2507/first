dc7f35d6ddc9db52df80cfedc23696eb32fe394c
Ha Nhat Lenh <lenh@ktc.local>
2017-08-24 19:05:41 +0900

    Commited from lenh at Thu, Aug 24, 2017  7:05:41 PM

--- modules/core/client/app/template.js
+++ modules/core/client/app/template.js
@@ -4,6 +4,5 @@ angular.module(ApplicationConfiguration.applicationModuleName).run(templateConfi
 
 templateConfig.$inject = ['$http', '$templateCache'];
 function templateConfig($http, $templateCache) {
-  $templateCache.put('poll_item', 'modules/polls/client/views/quick-poll.client.view.html');
   $http.get('modules/polls/client/views/quick-poll.client.view.html', { cache: $templateCache });
 }

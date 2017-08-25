727f4af0d79b38257f95661530f677b8cc17ba62
Ha Nhat Lenh <lenh@ktc.local>
2017-08-25 10:53:20 +0900

    Commited from lenh at Fri, Aug 25, 2017 10:53:20 AM

--- modules/polls/client/controllers/quick-poll.client.controller.js
+++ modules/polls/client/controllers/quick-poll.client.controller.js
@@ -34,7 +34,7 @@
       opts: [{
         color: Constants.colors[5]
       }, {
-        color: Constants.colors[40]
+        color: Constants.colors[65]
       }],
       category: ctrl.categorys[0]._id
     };

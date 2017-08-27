3b9163884dc98bba46f1a4dc41ee4ba6a2b79a28
acenter2507 <acenter2507@gmail.com>
2017-08-27 15:30:22 +0900

    Commited from HaNhatLenh at Sun, Aug 27, 2017  3:30:22 PM

---
+++ config/jobs/rank-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  path = require('path'),
+  User = mongoose.model('User'),
+  // Poll = mongoose.model('Poll'),
+  // Opt = mongoose.model('Opt'),
+  // Cmt = mongoose.model('Cmt'),
+  // Vote = mongoose.model('Vote'),
+  // Voteopt = mongoose.model('Voteopt'),
+  // Polltag = mongoose.model('Polltag'),
+  // Polluser = mongoose.model('Polluser'),
+  // Report = mongoose.model('Report'),
+  // Bookmark = mongoose.model('Bookmark'),
+  // Category = mongoose.model('Category'),
+  // View = mongoose.model('View'),
+  // Like = mongoose.model('Like'),
+  Userreport = mongoose.model('Userreport');
+
+exports.excute = function () {
+  get_users()
+    .then(users => {
+      console.log(users);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function get_users() {
+  return new Promise((resolve, reject) => {
+    var users = User.query({ 'roles': { '$not': { '$all': ['user', 'admin'] } } })
+      .then(users => {
+        return resolve(users);
+      }, err => {
+        return reject(err);
+      });
+  });
+}
--- config/lib/job.js
+++ config/lib/job.js
@@ -1,28 +1,26 @@
 'use strict';
 
-var CronJob = require('cron').CronJob;
+var CronJob = require('cron').CronJob,
+  rank_user = require('../jobs/rank-user');
+
 
 var test_job = new CronJob({
   cronTime: '* * * * *',
   onTick: function() {
-    console.log('Job running');
+    rank_user.excute();
   },
   start: false,
   timeZone: 'Asia/Tokyo'
 });
-// var rank_user_job = new CronJob({
-//   cronTime: '00 30 11 * * 0',
-//   onTick: function() {
-//     /*
-//      * Runs every Sunday at 11:30:00 AM
-//      */
-
-//   },
-//   start: false,
-//   timeZone: 'America/Los_Angeles'
-// });
+var rank_user_job = new CronJob({
+  cronTime: '0 0 * * *', //every 24hours (every midnight)
+  onTick: function() {
+    rank_user.excute();
+  },
+  start: false,
+  timeZone: 'Asia/Tokyo'
+});
 function start() {
-  console.log('run test job', test_job);
   test_job.start();
 }
 exports.start = start;

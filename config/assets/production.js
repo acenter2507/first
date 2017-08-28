e6f723084b6c77ab9f51de8d8797d2583daa7469
Ha Nhat Lenh <lenh@ktc.local>
2017-08-28 12:12:06 +0900

    Commited from lenh at Mon, Aug 28, 2017 12:12:06 PM

--- config/assets/production.js
+++ config/assets/production.js
@@ -56,7 +56,8 @@ module.exports = {
         'public/lib/angular-loading-bar/build/loading-bar.js',
         'public/lib/angular-breadcrumb/dist/angular-breadcrumb.min.js',
         'public/lib/angular-socialshare/dist/angular-socialshare.js',
-        'public/lib/angular-translate/angular-translate.min.js'
+        'public/lib/angular-translate/angular-translate.min.js',
+        'public/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js'
       ]
     },
     css: 'public/dist/application.min.css',

391435875f328149759a0c179d696bd5eed907c2
Ha Nhat Lenh <lenh@ktc.local>
2017-08-28 13:46:03 +0900

    Commited from lenh at Mon, Aug 28, 2017  1:46:03 PM

--- modules/polls/client/polls.client.module.js
+++ modules/polls/client/polls.client.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   app.registerModule('polls');
-  app.registerModule('polls.admin', ['core' ,'admin']);
+  app.registerModule('polls.admin', ['admin']);
   app.registerModule('polls.admin.routes', ['admin.routes']);
 }(ApplicationConfiguration));

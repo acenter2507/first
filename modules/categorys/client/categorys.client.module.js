609732dfa236300c40d631a17060e1b009112f87
HNL <acenter2507@gmail.com>
2017-08-28 23:27:48 +0900

    Commited from hanhatlenh at Mon Aug 28 23:27:48 JST 2017

--- modules/categorys/client/categorys.client.module.js
+++ modules/categorys/client/categorys.client.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   app.registerModule('categorys');
-  app.registerModule('categorys.admin', ['admin']);
+  app.registerModule('categorys.admin', ['core', 'admin']);
   app.registerModule('categorys.admin.routes', ['admin.routes']);
 }(ApplicationConfiguration));

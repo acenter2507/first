33bc23d60fdc29d899c2657a90edf7c96eb7d7b9
Ha Nhat Lenh <lenh@ktc.local>
2017-09-01 11:59:05 +0900

    Commited from lenh at Fri, Sep 01, 2017 11:59:05 AM

--- modules/users/client/services/password-validator.client.service.js
+++ modules/users/client/services/password-validator.client.service.js
@@ -7,39 +7,18 @@ angular.module('users').factory('PasswordValidator', ['$window', '$translate',
 
     return {
       getResult: function (password) {
-        return new Promise((resolve, reject) => {
-          var result = { errors: [] };
-          var check = 0;
-          if (password.length < 8) {
-            $translate('MS_PASS_MINLENGTH_ERROR').then(tsl => {
-              result.errors.push(tsl);
-              check += 1;
-            });
-          } else {
-            check += 1;
-          }
-          if (password.length > 32) {
-            $translate('MS_PASS_MAXLENGTH_ERROR').then(tsl => {
-              result.errors.push(tsl);
-              check += 1;
-            });
-          } else {
-            check += 1;
-          }
-          if(/^[a-zA-Z0-9- ]*$/.test(password) === false) {
-            $translate('MS_PASS_ILLEGAL_ERROR').then(tsl => {
-              result.errors.push(tsl);
-              check += 1;
-            });
-          } else {
-            check += 1;
-          }
-          while(true) {
-            if (check === 3) {
-              return resolve(result);
-            }
-          }
-        });
+        var result = { errors: [] };
+        var check = 0;
+        if (password.length < 8) {
+          result.errors.push('MS_PASS_MINLENGTH_ERROR');
+        }
+        if (password.length > 32) {
+          result.errors.push('MS_PASS_MAXLENGTH_ERROR');
+        }
+        if (/^[a-zA-Z0-9- ]*$/.test(password) === false) {
+          result.errors.push('MS_PASS_ILLEGAL_ERROR');
+        }
+        return result;
       },
       getPopoverMsg: function () {
         var popoverMsg = 'Please enter a passphrase or password with greater than 10 characters, numbers, lowercase, upppercase, and special characters.';

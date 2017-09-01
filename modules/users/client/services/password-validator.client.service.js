346faf69e90a8e99eeb71d557ec106d21aea47c0
Ha Nhat Lenh <lenh@ktc.local>
2017-09-01 11:52:04 +0900

    Commited from lenh at Fri, Sep 01, 2017 11:52:04 AM

--- modules/users/client/services/password-validator.client.service.js
+++ modules/users/client/services/password-validator.client.service.js
@@ -1,15 +1,45 @@
 'use strict';
 
 // PasswordValidator service used for testing the password strength
-angular.module('users').factory('PasswordValidator', ['$window',
-  function ($window) {
+angular.module('users').factory('PasswordValidator', ['$window', '$translate',
+  function ($window, $translate) {
     var owaspPasswordStrengthTest = $window.owaspPasswordStrengthTest;
 
     return {
       getResult: function (password) {
-        var result = owaspPasswordStrengthTest.test(password);
-        console.log(result);
-        return result;
+        return new Promise((resolve, reject) => {
+          var result = { errors: [] };
+          var check = 0;
+          if (password.length < 8) {
+            $translate('MS_PASS_MINLENGTH_ERROR').then(tsl => {
+              result.errors.push[tsl];
+              check += 1;
+            });
+          } else {
+            check += 1;
+          }
+          if (password.length > 32) {
+            $translate('MS_PASS_MAXLENGTH_ERROR').then(tsl => {
+              result.errors.push[tsl];
+              check += 1;
+            });
+          } else {
+            check += 1;
+          }
+          if(/^[a-zA-Z0-9- ]*$/.test(str) === false) {
+            $translate('MS_PASS_ILLEGAL_ERROR').then(tsl => {
+              result.errors.push[tsl];
+              check += 1;
+            });
+          } else {
+            check += 1;
+          }
+          while(true) {
+            if (check === 3) {
+              return resolve(result);
+            }
+          }
+        });
       },
       getPopoverMsg: function () {
         var popoverMsg = 'Please enter a passphrase or password with greater than 10 characters, numbers, lowercase, upppercase, and special characters.';

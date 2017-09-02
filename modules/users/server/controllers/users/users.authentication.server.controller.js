b4f9c55d5b87510499989dff5d180b903676d433
acenter2507 <acenter2507@gmail.com>
2017-09-02 23:56:49 +0900

    Commited from HaNhatLenh at Sat, Sep 02, 2017 11:56:49 PM

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -39,6 +39,7 @@ exports.signup = function (req, res) {
           message: 'LB_USER_EMAIL_INVALID'
         });
       User.findOne({ email: user.email }, function (err, user) {
+        console.log(err);
         if (user) {
           // Kiểm tra trạng thái user đã active
           if (user.status === 1)
@@ -55,6 +56,7 @@ exports.signup = function (req, res) {
     },
     function (done) {
       crypto.randomBytes(20, function (err, buffer) {
+        console.log(err);
         if (err)
           return res.status(400).send({
             message: 'MS_CM_LOAD_ERROR'
@@ -67,6 +69,7 @@ exports.signup = function (req, res) {
       user.activeAccountToken = token;
       user.activeAccountExpires = Date.now() + 1800000; //86400000; // 24h
       user.save(function (err, _user) {
+        console.log(err);
         if (err)
           return res.status(400).send({
             message: errorHandler.getErrorMessage(err)
@@ -87,6 +90,7 @@ exports.signup = function (req, res) {
         appName: config.app.title,
         url: httpTransport + req.headers.host + '/api/auth/verify/' + token
       }, function (err, emailHTML) {
+        console.log(err);
         done(err, emailHTML, user);
       });
     },
@@ -101,6 +105,7 @@ exports.signup = function (req, res) {
         if (!err) {
           return res.redirect('/authentication/send');
         } else {
+          console.log(err);
           return res.status(400).send({
             message: 'MS_USERS_SEND_FAIL'
           });

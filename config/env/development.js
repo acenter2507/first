469337cbba4e51ac12f6866b45ba19c046af3015
acenter2507 <acenter2507@gmail.com>
2017-09-03 13:33:09 +0900

    Commited from HaNhatLenh at Sun, Sep 03, 2017  1:33:09 PM

--- config/env/development.js
+++ config/env/development.js
@@ -67,14 +67,14 @@ module.exports = {
   },
   mailer: {
     account: {
-      from: process.env.ACCOUNT_MAILER_FROM || 'support@cadobongdafree.com',
+      from: process.env.ACCOUNT_MAILER_FROM || 'acenter2507@gmail.com',
       options: {
         // service: process.env.ACCOUNT_MAILER_SERVICE_PROVIDER || 'Gmail',
-        host: process.env.ACCOUNT_MAILER_HOST || 'mx1.hostinger.vn',
-        port: process.env.ACCOUNT_MAILER_PORT || '587',
+        host: process.env.ACCOUNT_MAILER_HOST || 'smtp.gmail.com',
+        port: process.env.ACCOUNT_MAILER_PORT || '465',
         secure: process.env.ACCOUNT_MAILER_SECURE === 'true',
         auth: {
-          user: process.env.ACCOUNT_MAILER_EMAIL_ID || 'support@cadobongdafree.com',
+          user: process.env.ACCOUNT_MAILER_EMAIL_ID || 'acenter2507',
           pass: process.env.ACCOUNT_MAILER_PASSWORD || '1@Aa1@Aa'
         }
       }

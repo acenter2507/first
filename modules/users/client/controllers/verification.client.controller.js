526e571189dfc3e82f93b2e315c1bcd843c4c8a2
acenter2507 <acenter2507@gmail.com>
2017-09-03 15:30:08 +0900

    Commited from HaNhatLenh at Sun, Sep 03, 2017  3:30:08 PM

--- modules/users/client/controllers/verification.client.controller.js
+++ modules/users/client/controllers/verification.client.controller.js
@@ -11,13 +11,13 @@ function VerificationController(
 ) {
   $scope.error = $location.search().err;
   $scope.message = '';
-  if (error === 1 || error === '1') {
+  if ($scope.error === 1 || $scope.error === '1') {
     $scope.message = 'MS_USERS_VERIFI_ERROR_1';
   }
-  if (error === 2 || error === '2') {
+  if ($scope.error === 2 || $scope.error === '2') {
     $scope.message = 'MS_USERS_BLOCK';
   }
-  if (error === 3 || error === '3') {
+  if ($scope.error === 3 || $scope.error === '3') {
     $scope.message = 'MS_USERS_VERIFI_ERROR_3';
   }
 }

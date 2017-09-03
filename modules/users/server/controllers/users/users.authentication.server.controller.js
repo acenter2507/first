c17dd363d242ffd9982b6875aa4982a0a43f7bd6
acenter2507 <acenter2507@gmail.com>
2017-09-03 14:09:07 +0900

    Commited from HaNhatLenh at Sun, Sep 03, 2017  2:09:07 PM

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -169,6 +169,7 @@ exports.signup = function (req, res) {
     // }
   ], function (err) {
     if (err) {
+      console.log(err);
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
       });

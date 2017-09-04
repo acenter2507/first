98ffedc80a6e85e1a8cb08e2ca519b44b54f3d95
acenter2507 <acenter2507@gmail.com>
2017-09-04 23:50:51 +0900

    Commited from HaNhatLenh at Mon, Sep 04, 2017 11:50:51 PM

--- config/env/development.js
+++ config/env/development.js
@@ -72,7 +72,8 @@ module.exports = {
         // service: process.env.ACCOUNT_MAILER_SERVICE_PROVIDER || 'Gmail',
         host: process.env.ACCOUNT_MAILER_HOST || 'mx1.hostinger.vn',
         port: process.env.ACCOUNT_MAILER_PORT || '587',
-        secure: process.env.ACCOUNT_MAILER_SECURE === 'true',
+        secure: true,
+        tls: {cipher:'SSLv3'},
         auth: {
           user: process.env.ACCOUNT_MAILER_EMAIL_ID || 'account@cadobongdafree.com',
           pass: process.env.ACCOUNT_MAILER_PASSWORD || '1aA@1aA@'

071a515afd73e79a71453aac01f2e681070448d3
acenter2507 <acenter2507@gmail.com>
2017-09-05 21:58:22 +0900

    Commited from HaNhatLenh at Tue, Sep 05, 2017  9:58:22 PM

--- config/env/production.js
+++ config/env/production.js
@@ -69,16 +69,16 @@ module.exports = {
   },
   mailer: {
     account: {
-      from: process.env.ACCOUNT_MAILER_FROM || 'account@cadobongdafree.com',
+      from: process.env.ACCOUNT_MAILER_FROM || 'acenter2507@gmail.com',//'account@cadobongdafree.com',
       options: {
-        // service: process.env.ACCOUNT_MAILER_SERVICE_PROVIDER || 'Gmail',
-        host: process.env.ACCOUNT_MAILER_HOST || 'mx1.hostinger.vn',
-        port: process.env.ACCOUNT_MAILER_PORT || '587',
+        service: process.env.ACCOUNT_MAILER_SERVICE_PROVIDER || 'gmail',
+        host: process.env.ACCOUNT_MAILER_HOST || 'smtp.gmail.com', //'mx1.hostinger.vn',
+        port: process.env.ACCOUNT_MAILER_PORT || '465',// '587',
         secure: true,
-        tls: { rejectUnauthorized: false },
+        // tls: { rejectUnauthorized: false },
         auth: {
-          user: process.env.ACCOUNT_MAILER_EMAIL_ID || 'account@cadobongdafree.com',
-          pass: process.env.ACCOUNT_MAILER_PASSWORD || '1aA@1aA@'
+          user: process.env.ACCOUNT_MAILER_EMAIL_ID || 'acenter2507@gmail.com',//'account@cadobongdafree.com',
+          pass: process.env.ACCOUNT_MAILER_PASSWORD || '1@Aa1@Aa'//'1aA@1aA@'
         }
       }
     },

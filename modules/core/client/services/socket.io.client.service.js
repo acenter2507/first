b96e01e771b27281c6932ae76caab2076b8c7f48
Ha Nhat Lenh <lenh@ktc.local>
2017-09-06 18:10:12 +0900

    Commited from lenh at Wed, Sep 06, 2017  6:10:12 PM

--- modules/core/client/services/socket.io.client.service.js
+++ modules/core/client/services/socket.io.client.service.js
@@ -1,51 +1,61 @@
 'use strict';
+angular.module('core')
+  .service('mSocketFactory', mSocketFactory)
+  .service('Socket', Socket);
 
-// Create the Socket.io wrapper service
-angular.module('core').service('Socket', [
-  'Authentication',
-  '$timeout',
-  'socketFactory',
-  '$location',
-  function(Authentication, $timeout, socketFactory, $location) {
-    // Connect to Socket.io server
-    this.connect = function() {
-      var protocol = $location.protocol();
-      var host = $location.host();
-      var port = $location.port();
-      var url = protocol + '://' + host + ((port !== '') ? ':' + port : '');
-      console.log(url);
-      this.socket = socketFactory();
-      if (Authentication.user) {
-        this.socket.emit('init', { user: Authentication.user._id });
-      } else {
-        this.socket.emit('guest');
-      }
-    };
-    this.connect();
+mSocketFactory.$inject = ['socketFactory',];
+function mSocketFactory(socketFactory) {
+  return function (opts) {
+    opts.ioSocket = opts.ioSocket || io.connect();
+    var socket = socketFactory(opts);
+    socket.socket = opts.ioSocket;
+    return socket;
+  };
+}
 
-    // Wrap the Socket.io 'on' method
-    this.on = function(eventName, callback) {
-      if (this.socket) {
-        this.socket.on(eventName, function(data) {
-          $timeout(function() {
-            callback(data);
-          });
+Socket.$inject = ['Authentication', '$timeout', 'mSocketFactory', '$location',];
+function Socket(Authentication, $timeout, mSocketFactory, $location) {
+  // Connect to Socket.io server
+  this.connect = function () {
+    var protocol = $location.protocol();
+    var host = $location.host();
+    var port = $location.port();
+    var url = protocol + '://' + host + ((port !== '') ? (':' + port) : '');
+    console.log(url);
+    this.socket = mySocketFactory({
+      ioSocket: io.connect(url)
+    });
+    // this.socket = socketFactory();
+    if (Authentication.user) {
+      this.socket.emit('init', { user: Authentication.user._id });
+    } else {
+      this.socket.emit('guest');
+    }
+  };
+  this.connect();
+
+  // Wrap the Socket.io 'on' method
+  this.on = function (eventName, callback) {
+    if (this.socket) {
+      this.socket.on(eventName, function (data) {
+        $timeout(function () {
+          callback(data);
         });
-      }
-    };
+      });
+    }
+  };
 
-    // Wrap the Socket.io 'emit' method
-    this.emit = function(eventName, data) {
-      if (this.socket) {
-        this.socket.emit(eventName, data);
-      }
-    };
+  // Wrap the Socket.io 'emit' method
+  this.emit = function (eventName, data) {
+    if (this.socket) {
+      this.socket.emit(eventName, data);
+    }
+  };
 
-    // Wrap the Socket.io 'removeListener' method
-    this.removeListener = function(eventName) {
-      if (this.socket) {
-        this.socket.removeListener(eventName);
-      }
-    };
-  }
-]);
+  // Wrap the Socket.io 'removeListener' method
+  this.removeListener = function (eventName) {
+    if (this.socket) {
+      this.socket.removeListener(eventName);
+    }
+  };
+}

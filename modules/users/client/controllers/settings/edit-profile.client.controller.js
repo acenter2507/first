0ef14903d297522eea61fc81f2162c16ba1db6d4
acenter2507 <acenter2507@gmail.com>
2017-09-12 23:12:28 +0900

    Commited from HaNhatLenh at Tue, Sep 12, 2017 11:12:28 PM

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -136,6 +136,7 @@ function EditProfileController(
       });
       mDialog.closePromise.then(function (data) {
         console.log(data);
+        console.log($scope.uploader);
       });
     }
   };

28aac725941a72f976dfefb0e319d1e2ad2682b6
acenter2507 <acenter2507@gmail.com>
2017-09-18 03:02:46 +0900

    Commited from HaNhatLenh at Mon, Sep 18, 2017  3:02:46 AM

--- modules/core/client/controllers/others/contact-support.client.controller.js
+++ modules/core/client/controllers/others/contact-support.client.controller.js
@@ -52,7 +52,8 @@ angular.module('core').controller('ContactSupportController', [
 
     $scope.ticketTest = {};
     $scope.test = {};
-    $scope.test.momentTest = moment().local();
+    $scope.test.momentUTC = moment.utc();
+    $scope.test.momentLocal = moment.local();
     $scope.test.language = $translate.use();
 
     $scope.setLocale = (lang) => {
@@ -61,7 +62,8 @@ angular.module('core').controller('ContactSupportController', [
         moment.tz.setDefault(tz);
         moment.locale(lang);
         amMoment.changeLocale(lang);
-        $scope.test.momentTest = moment();
+        $scope.test.momentUTC = moment.utc();
+        $scope.test.momentLocal = moment.local();
         $scope.test.language = $translate.use();
         if (!$scope.$$phase) $scope.$digest();
       });

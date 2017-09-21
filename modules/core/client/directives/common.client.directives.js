97e49c7d1fd296b750b4a449fc2d1ad8dccfefc3
Ha Nhat Lenh <lenh@ktc.local>
2017-09-21 12:04:12 +0900

    Commited from lenh at Thu, Sep 21, 2017 12:04:12 PM

--- modules/core/client/directives/common.client.directives.js
+++ modules/core/client/directives/common.client.directives.js
@@ -3,6 +3,7 @@ angular
   .module('core')
   .directive('a', preventClickDirective)
   .directive('a', cardExpandDirective)
+  .directive('copyToClipboard', copyToClipboardDirective)
   .directive('a', asideMenuToggleDirective)
   .directive('body', asideMenuHideDirective)
   .directive('backToTop', backToTopDirective)
@@ -45,6 +46,56 @@ function cardExpandDirective() {
     });
   }
 }
+
+//Card Collapse
+function copyToClipboardDirective() {
+  var clip;
+  function link(scope, element) {
+    function clipboardSimulator() {
+      var self = this,
+        textarea,
+        container;
+      function createTextarea() {
+        if (!self.textarea) {
+          container = document.createElement('div');
+          container.id = 'simulate-clipboard-container';
+          container.setAttribute('style', ['position: fixed;', 'left: 0px;', 'top: 0px;', 'width: 0px;', 'height: 0px;', 'z-index: 100;', 'opacity: 0;', 'display: block;'].join(''));
+          document.body.appendChild(container);
+          textarea = document.createElement('textarea');
+          textarea.setAttribute('style', ['width: 1px;', 'height: 1px;', 'padding: 0px;'].join(''));
+          textarea.id = 'simulate-clipboard';
+          self.textarea = textarea;
+          container.appendChild(textarea);
+        }
+      }
+      createTextarea();
+    }
+    clipboardSimulator.prototype.copy = function () {
+      this.textarea.innerHTML = '';
+      this.textarea.appendChild(document.createTextNode(scope.textToCopy));
+      this.textarea.focus();
+      this.textarea.select();
+      setTimeout(function () {
+        document.execCommand('copy');
+      }, 20);
+    };
+    clip = new clipboardSimulator();
+    element[0].addEventListener('click', function () {
+      clip.copy();
+    });
+
+  }
+
+  var directive = {
+    restrict: 'A',
+    link: link,
+    scope: {
+      textToCopy: '='
+    }
+  };
+  return directive;
+}
+
 
 //LayoutToggle
 asideMenuToggleDirective.$inject = ['$interval'];

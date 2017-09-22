75f99a40b31ee3cfc72d32c5ba95af3c5d9f558f
Ha Nhat Lenh <lenh@ktc.local>
2017-09-22 18:46:42 +0900

    Commited from lenh at Fri, Sep 22, 2017  6:46:42 PM

--- modules/polls/client/controllers/report-poll.client.controller.js
+++ modules/polls/client/controllers/report-poll.client.controller.js
@@ -29,6 +29,12 @@
     ctrl.monthCnt = 0;
     ctrl.weekCnt = 0;
 
+    // Chart data
+    ctrl.mode = 1; // 1: month - 2: year - 3: weeks
+    ctrl.year = '';
+    ctrl.month = '';
+    ctrl.series = ['Thành viên', 'Khách'];
+
 
     onPrepare();
 
@@ -95,24 +101,57 @@
     function prepareYears() {
       ctrl.yearCnt = ctrl.endDate.diff(ctrl.startDate, 'years');
       for (var index = 0; index < ctrl.yearCnt; index++) {
-        var item = ctrl.startDate.clone().add(index, 'years').format('YYYY');
+        var item = ctrl.startDate.clone().add(index, 'years').year();
         ctrl.years.push(item);
       }
-      console.log(ctrl.years);
+      ctrl.year = ctrl.years[ctrl.years.length - 1];
     }
     function prepareMonths() {
       ctrl.monthCnt = ctrl.endDate.diff(ctrl.startDate, 'months');
       for (var index = 0; index <= ctrl.monthCnt; index++) {
-        var item = ctrl.startDate.clone().add(index, 'months').format('YYYY-MM');
+        var item = ctrl.startDate.clone().add(index, 'months').month();
         ctrl.months.push(item);
       }
-      console.log(ctrl.months);
+      ctrl.month = ctrl.months[ctrl.months.length - 1];
     }
     function prepareWeeks() {
       ctrl.weekCnt = ctrl.endDate.diff(ctrl.startDate, 'weeks');
     }
     function prepareReportTraffic() {
       ctrl.traffic = {};
+      ctrl.traffic.labels = 1;
+
+    }
+
+    /**
+     * HANDLES
+     */
+    ctrl.handleChangeYear = handleChangeYear;
+    function handleChangeYear() {
+      ctrl.months = handleGetMonthsOfYear(ctrl.year);
+      ctrl.month = ctrl.months[ctrl.months.length - 1];
+    }
+    ctrl.handleChangeMonth = handleChangeMonth;
+    function handleChangeMonth() {
+    }
+    function handleGetMonthsOfYear(year) {
+      var year = moment().utc().year(year);
+      var startMonth = year.clone().startOf('year');
+      var endMonth = year.clone().endOf('year').subtract(1, 'days');
+      var durration = endMonth.diff(startMonth, 'months');
+      var months = [];
+      for (var index = 0; index <= durration; index++) {
+        var item = startMonth.clone().add(index, 'months').month();
+        months.push(item);
+      }
+      year = undefined;
+      startMonth = undefined;
+      endMonth = undefined;
+      return months;
+    }
+    function handleGetDatesOfMonth(month) {
+      var startDate = month.clone().startOf('month');
+      var endDate = month.clone().startOf('month');
     }
   }
 })();

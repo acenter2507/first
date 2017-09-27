63353f7deb37ccdac7149779d99a45868561e1bf
acenter2507 <acenter2507@gmail.com>
2017-09-28 01:09:46 +0900

    Commited from HaNhatLenh at Thu, Sep 28, 2017  1:09:46 AM

--- modules/polls/server/controllers/polls.server.controller.js
+++ modules/polls/server/controllers/polls.server.controller.js
@@ -295,7 +295,7 @@ exports.pollByID = function (req, res, next, id) {
  * Lấy danh sách poll cho màn hình polls.list
  */
 exports.findPolls = function (req, res) {
-  getClientIp(req);
+  console.log(getClientIp(req));
   var page = req.params.page || 0;
   var userId = req.user ? req.user._id : undefined;
   // Lấy ngôn ngữ hiển thị poll

46bf2f43da56b752a9da1037580d061ff835681d
acenter2507 <acenter2507@gmail.com>
2017-09-28 22:11:48 +0900

    Commited from HaNhatLenh at Thu, Sep 28, 2017 10:11:48 PM

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -130,14 +130,8 @@
         type: 'pie',
         options: {
           responsive: true,
-          scales:
-          {
-            xAxes: [{
-              display: false
-            }],
-            yAxes: [{
-              display: false
-            }],
+          legend: {
+            display: false
           }
         },
         colors: [],

47a7a0b9f5f9736caed413d3790a1d9557ef753a
Ha Nhat Lenh <lenh@ktc.local>
2017-09-30 14:37:48 +0900

    Commited from lenh at Sat, Sep 30, 2017  2:37:48 PM

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -138,7 +138,7 @@
       ctrl.voteopts = handleGetVoteOptionInVote(ctrl.votes);
       ctrl.votedTotal = ctrl.voteopts.length;
       ctrl.opts.forEach(opt => {
-        opt.voteCnt = _.where(ctrl.voteopts, { opt: opt._id }).length || 0;
+        opt.voteCnt = _.find(ctrl.voteopts, (optId) => { return optId.toString() === opt._id.toString() }).length || 0;
         opt.progressVal = Action.calPercen(ctrl.votedTotal, opt.voteCnt);
         ctrl.chart.colors.push(opt.color);
         ctrl.chart.labels.push(opt.title);
@@ -150,7 +150,6 @@
       votes.forEach(vote => {
         voteOpts = _.union(voteOpts, vote.opts);
       });
-      console.log(voteOpts);
       return voteOpts;
     }
     function prepareOwnerInfo() {

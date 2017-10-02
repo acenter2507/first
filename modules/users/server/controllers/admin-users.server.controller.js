450d3abce55aa360771778fe228ff84b5d6d95dd
Ha Nhat Lenh <lenh@ktc.local>
2017-10-02 12:50:15 +0900

    Commited from lenh at Mon, Oct 02, 2017 12:50:15 PM

--- modules/users/server/controllers/admin-users.server.controller.js
+++ modules/users/server/controllers/admin-users.server.controller.js
@@ -213,6 +213,7 @@ exports.userByID = function (req, res, next, id) {
  * Lấy all users
  */
 exports.loadAdminUsers = function (req, res) {
+  console.log(req.body);
   var page = req.body.page || 1;
   var condition = req.body.condition || {};
   var sort = condition.sort || '-created';

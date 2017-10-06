d03a4b64c0417d4d785cbf8b34c7750664ca7ea9
Ha Nhat Lenh <lenh@ktc.local>
2017-10-06 14:31:19 +0900

    Commited from lenh at Fri, Oct 06, 2017  2:31:19 PM

--- modules/views/server/models/view.server.model.js
+++ modules/views/server/models/view.server.model.js
@@ -4,24 +4,17 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+  paginate = require('mongoose-paginate'),
   Schema = mongoose.Schema;
 
 /**
  * View Schema
  */
 var ViewSchema = new Schema({
-  poll: {
-    type: Schema.ObjectId,
-    ref: 'Poll'
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+  poll: { type: Schema.ObjectId, ref: 'Poll' },
+  created: { type: Date, default: Date.now },
+  user: { type: Schema.ObjectId, ref: 'User' }
 });
+ViewSchema.plugin(paginate);
 
 mongoose.model('View', ViewSchema);

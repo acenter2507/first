bf640aa1a365967949012e25a009989a7e626d81
Ha Nhat Lenh <lenh@ktc.local>
2017-10-11 10:25:15 +0900

    Commited from lenh at Wed, Oct 11, 2017 10:25:15 AM

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -31,7 +31,6 @@ angular.module('core').controller('HeaderController', [
       // Nghe sự kiện chuyển thay đổi ngôn ngữ
       $scope.$on('changeLanguage', (event, args) => {
         $scope.language = args.language;
-        console.log('Header: changeLanguage: ', args.language);
       });
     }
     // Đánh dấu tất cả notifs thành đã xem

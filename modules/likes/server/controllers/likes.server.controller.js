8add8f720ba5a459869d4ebae096f9d98a5d2730
Ha Nhat Lenh <lenh@ktc.local>
2017-10-11 10:41:09 +0900

    Commited from lenh at Wed, Oct 11, 2017 10:41:09 AM

--- modules/likes/server/controllers/likes.server.controller.js
+++ modules/likes/server/controllers/likes.server.controller.js
@@ -103,6 +103,7 @@ exports.delete = function (req, res) {
     }, handleError);
 
   function handleError(err) {
+    console.log(err);
     return res.status(400).send({
       message: errorHandler.getErrorMessage(err)
     });

f7a68125a14eed27f0b551e754e255350de8b40d
Ha Nhat Lenh <lenh@ktc.local>
2017-11-02 11:39:59 +0900

    Commited from lenh at Thu, Nov 02, 2017 11:39:59 AM

--- modules/polls/server/controllers/polls.server.controller.js
+++ modules/polls/server/controllers/polls.server.controller.js
@@ -628,13 +628,13 @@ function search_condition_analysis(condition, userId) {
   }
   if (condition.created_start) {
     console.log(condition.created_start);
-    let start = new _moment(condition.created_start).utc().startOf('day').format();
+    let start = new _moment(condition.created_start).utc().startOf('day').add(1, 'day').format();
     console.log(start);
     and_arr.push({ created: { $gte: start } });
   }
   if (condition.created_end) {
     console.log(condition.created_end);
-    let end = new _moment(condition.created_end).utc().endOf('day').format();
+    let end = new _moment(condition.created_end).utc().endOf('day').add(1, 'day').format();
     console.log(end);
     and_arr.push({ created: { $lt: end } });
   }
